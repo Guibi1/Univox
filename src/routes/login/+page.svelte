a4524a6a9aa89eadefcc78a92eb25725ecf1c3a3
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-01-25 10:39:26 -0500

    Trash login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,24 @@
+<main class="h-screen flex justify-evenly items-center bg-stone-200">
+	<div class="flex flex-col gap-4">
+		<p class="h-28 w-28 bg-cyan-100">LOGO</p>
+		<h1 class="font-bold">Univox</h1>
+	</div>
+
+	<form class="flex flex-col justify-between w-96 h-max gap-6 bg-white p-12 rounded">
+		<h2>Connexion</h2>
+
+		<label class="flex flex-col gap w-full">
+			Numéro de DA
+			<input type="text" class="h-10" />
+		</label>
+
+		<label class="flex flex-col gap w-full">
+			Mot de passe
+			<input type="password" class="h-10" />
+		</label>
+
+		<button class="self-end text-cyan-300 cursor-pointer">Mot de passe oublié?</button>
+
+		<button type="submit" class="h-10 rounded bg-cyan-300">Se connecter</button>
+	</form>
+</main>
