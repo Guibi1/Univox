01f7ab6ab717d35477587dfc055696b89bda48da
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-01-27 12:14:02 -0500

    Big login changes

---
+++ src/routes/(auth)/connexion/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { user } from '../../../stores';
+	let email = '';
+	let password = '';
+</script>
+
+<form on:submit={() => user.login(email, password)} class="flex flex-col justify-between gap-6">
+	<h1 class="text-center">Connexion</h1>
+
+	<label>
+		Courriel
+		<input bind:value={email} type="email" class="h-10" />
+	</label>
+
+	<label>
+		Mot de passe
+		<input bind:value={password} type="password" class="h-10" />
+	</label>
+
+	<div class="flex justify-between">
+		<a href="/inscription">S'inscrire</a>
+		<a href="/mot-de-passe-oublié">Mot de passe oublié ?</a>
+	</div>
+
+	<button type="submit">Se connecter</button>
+</form>
