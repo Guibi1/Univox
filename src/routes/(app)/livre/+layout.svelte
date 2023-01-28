dce16c744fb92bfb0b52c75f62d793b6edc9100a
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-01-28 17:05:54 -0500

    Added a button to achat and vendre, its buggy af
    It's almost as buggy as a moon buggy.

--- src/routes/(app)/livre/+layout.svelte
+++ src/routes/(app)/livre/+layout.svelte
@@ -1,8 +1,45 @@
-<div class="topnav-centeredflex flex-col justify-between gap-6 bg-gray-700 h-2/4">
-    <a href="">About</a>
-    <a href="">Skill</a>
-    <a href="">Projects</a>
-    <a href="">Contact</a>
-</div>
+<script lang="ts">
+    import { page } from '$app/stores';
+    let vendre: boolean = true;
+    page.subscribe((page) => vendre = page.url.pathname.endsWith("/achat"));
+
+</script>
+
+<a href={vendre ? "mes-livres" : "achat"}>
+    <button> 
+        {#if vendre}
+            <box-icon name='book-add'></box-icon>
+            Vendre
+    
+            {:else}
+            <box-icon name='book-add'></box-icon>
+            Acheter
+        {/if}
+    </button>
+</a>
+
 
 <slot />
+
+
+
+
+<style> 
+    button {
+        @apply absolute rounded-full;
+        @apply right-12 bottom-10;
+
+        @apply gap-3 p-5;
+        @apply text-2xl w-44 h-14;
+
+        @apply text-black;
+        
+
+
+    }
+
+    box-icon {
+            @apply h-10 w-10;
+
+        }
+</style>
