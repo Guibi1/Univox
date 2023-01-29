8c0f5eb8b2aa33a34934efbf11da44f43e6ca70a
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-01-28 22:34:07 -0500

    Added a basic book page

---
+++ src/routes/(app)/livres/achat/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import BookFilter from "./BookFilter.svelte";
+    import BookList from "./BookList.svelte";
+
+    let books: Book[] = [
+        {
+            id: "1",
+            price: 10,
+            author: "Your mom",
+            description: "This is a good book",
+            title: "Book!",
+        },
+    ];
+    let filters: any = {};
+    console.log("🚀 ~ file: +layout.svelte:7 ~ filters", filters);
+</script>
+
+<div>
+    <h1 class="text-center">Achat</h1>
+
+    <main class="grid grid-cols-3 gap-4 p-6">
+        <BookFilter bind:filters />
+
+        <BookList {books} />
+
+        <slot />
+    </main>
+</div>
---
+++ src/routes/(app)/livres/achat/BookFilter.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let filters;
+
+    filters = { test: "asdfasd" };
+</script>
+
+<div>
+    <ul>
+        <li>matiere 1</li>
+        <li>matiere 2</li>
+        <li>matiere 3</li>
+        <li>matiere 4</li>
+    </ul>
+</div>
