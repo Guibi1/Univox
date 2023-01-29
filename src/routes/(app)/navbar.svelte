2bcf9fc1ed83a97b88cfc6eebda38f640b53b378
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-01-29 17:00:22 -0500

    Made Navbar pretty

--- src/routes/(app)/navbar.svelte
+++ src/routes/(app)/navbar.svelte
@@ -1,24 +1,36 @@
-<div class="h-8">
+<script lang="ts">
+    import { page } from "$app/stores";
+    import { onDestroy } from "svelte";
+
+    const pages = [
+        { href: "/", title: "Accueil" },
+        { href: "/livres", title: "Livres" },
+        { href: "/horaires", title: "Horaires" },
+        { href: "/amis", title: "Amis" },
+    ];
+
+    let pathname: string = "";
+    onDestroy(page.subscribe((page) => (pathname = page.url.pathname)));
+</script>
+
+<div class="h-10">
     <!-- This is a spacer for the top of the page -->
 </div>
 
-<nav class="absolute inset-0 bottom-auto flex flex-row gap-2 justify-between items-center p-1">
+<nav class="absolute inset-0 h-10 flex flex-row justify-between items-center bg-zinc-600">
     <div>LOGO</div>
-    <ul class="flex flex-row gap-2 justify-between items-center">
-        <li>
-            <a href="/livre/achat">Acheter un livre</a>
-        </li>
-        <li>
-            <a href="/connexion">Connexion</a>
-        </li>
+
+    <ul class="flex flex-row justify-between items-center">
+        {#each pages as page}
+            <li>
+                <a
+                    href={page.href}
+                    class={"flex items-center h-10 px-4 text-white text-center hover:text-black hover:bg-zinc-400 " +
+                        (pathname.startsWith(page.href) ? "bg-zinc-600" : "")}>{page.title}</a
+                >
+            </li>
+        {/each}
     </ul>
 </nav>
 
-<style>
-    li {
-    }
-
-    a {
-        @apply border-cyan-300 border-2;
-    }
-</style>
+<slot />
