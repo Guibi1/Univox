8536bc568cc4c0f148693b201397ee109166f79c
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-01-30 16:37:10 -0500

    updated temporarly the logo KEKW

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,7 +1,7 @@
 <div class="min-h-screen flex justify-evenly items-center bg-stone-200">
-    <div class="flex flex-col gap-4">
-        <p class="h-28 w-28 bg-cyan-100">LOGO</p>
-        <h1 class="font-bold">Univox</h1>
+    <div class="flex flex-col gap-4 items-center">
+        <img src="src\routes\(auth)\291968534f919686e016f74046fa6e5f.webp" alt="">
+        <h1 class="font-bold text-4xl items-center ">Univox</h1>
     </div>
 
     <main class="bg-white p-12 rounded w-96 h-max">
