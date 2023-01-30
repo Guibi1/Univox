04fac878a044589e47f038c29d170366de73b0c9
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-01-30 15:40:41 -0500

    better topnav, still bad

---
+++ src/routes/(app)/livres/nouveau-livre/+page@(app).svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    let titreLivre = "";
+    let etatLivre = "";
+    let prix: string = "";
+
+    function doSomething(titreLivre: any, etatLivre: any): any {
+        throw new Error("Function not implemented.");
+    }
+</script>
+
+<div class="min-h-screen flex justify-evenly items-center bg-stone-200">
+
+    <main class="bg-white p-12 rounded w-96 h-max">
+        
+        <form on:submit={() => doSomething(titreLivre, etatLivre)} class="flex flex-col justify-between gap-6">
+            <h1 class="text-center">Vendre un livre</h1>
+        
+            <label>
+                Nom du livre
+                <input bind:value={titreLivre} required />
+            </label>
+        
+            <label>
+                L'état du livre
+                <input bind:value={etatLivre} required />
+            </label>
+                <label >
+                    Prix de vente
+                    <input bind:value={prix} required />
+                </label>            
+        
+            <button type="submit">Créer l'annonce</button>
+        </form>
+    </main>
+</div>
