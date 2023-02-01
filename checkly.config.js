b965b70a3bb2810c17eae36ea238423fba21ef6e
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-02-01 17:59:19 -0500

    Fixed the url

--- checkly.config.js
+++ checkly.config.js
@@ -1,6 +1,6 @@
 const LOCAL_DEV_URL = "http://localhost:3000";
 const PREVIEW_URL = process.env.ENVIRONMENT_URL;
-const PROD_URL = "https://projet.guibi.com";
+const PROD_URL = "https://univox.guibi.ca";
 
 const config = {
     headless: process.env.NODE_ENV !== "development",
