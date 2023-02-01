216216b26a30a09b835b14b305792a4b0a73042d
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-02-01 13:42:34 -0500

    Fixedthe navbar current page

--- src/routes/(app)/Navbar.svelte
+++ src/routes/(app)/Navbar.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
     import { page } from "$app/stores";
-    import { onDestroy } from "svelte";
+    import logo from "$assets/logo.webp";
 
     const pages = [
         { href: "/", title: "Accueil" },
         { href: "/livres", title: "Livres" },
-        { href: "/horaires", title: "Horaires" },
+        { href: "/horaire", title: "Horaires" },
         { href: "/amis", title: "Amis" },
     ];
 
-    let pathname: string = "";
-    onDestroy(page.subscribe((page) => (pathname = page.url.pathname)));
+    let currentPage: number = 0;
+    $: {
+        for (let [i, { href }] of pages.entries()) {
+            if ($page.url.pathname.startsWith(href)) currentPage = i;
+        }
+    }
 </script>
 
 <div class="h-10">
@@ -18,15 +22,18 @@
 </div>
 
 <nav class="absolute inset-0 h-10 flex flex-row justify-between items-center bg-zinc-600">
-    <div>LOGO</div>
+    <div class="h-full flex flex-row items-center gap-2">
+        <img class="h-full aspect-square p-1" src={logo} alt="Univox's logo" />
+        <h1 class="text-white">UNIVOX</h1>
+    </div>
 
     <ul class="flex flex-row justify-between items-center">
-        {#each pages as page}
+        {#each pages as page, i}
             <li>
                 <a
                     href={page.href}
                     class={"flex items-center h-10 px-4 text-white text-center hover:text-black hover:bg-zinc-400 " +
-                        (pathname.startsWith(page.href) ? "bg-green-500" : "")}>{page.title}</a
+                        (i === currentPage ? "bg-green-500" : "")}>{page.title}</a
                 >
             </li>
         {/each}
