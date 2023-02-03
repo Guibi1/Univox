1bf0d5db07a36dbd491f5fc156e8e74810ce56fd
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-02-03 11:42:42 -0500

    Added darkmode to the site

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["src/app.html", "src/**/*.svelte"],
+    darkMode: ["class", '[data-colorscheme="dark"]'],
     theme: {
         extend: {},
     },
