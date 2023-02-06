44e5c000319f88a47eb0b01d8c875d05f725587c
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-02-06 16:23:16 -0500

    am dying trying to create a table

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,4 +1,54 @@
 <script lang="ts">
+    import { Weekday, type Class } from "$lib/Types";
+    import dayjs from "dayjs";
+
+    /*
+    export type Class = {
+    id: string;
+    name: string;
+    group: number;
+    local: string;
+    type: "T" | "L";
+    teacher: string;
+    virtual: boolean;
+    weekday: Weekday;
+    timeStart: Dayjs;
+    timeEnd: Dayjs;
+    };
+    */
+
+    let schedule: Class[] = [
+        {
+            id: "theIDLOL",
+            name: "aName",
+            group: 105,
+            local: "B-105",
+            type: "L",
+            teacher: "asdjkfwlbk",
+            virtual: false,
+            weekday: Weekday.Friday,
+            timeStart: dayjs().hour(12),
+            timeEnd: dayjs().hour(14),
+        },
+    ];
 </script>
 
-<h1 class="text-6xl">Univox</h1>
+<body>
+    <table>
+        <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+        </tr>
+        <tr class="text-center">
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+        </tr>
+        <tr class="text-center">
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+        </tr>
+    </table>
+</body>
