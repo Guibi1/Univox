56f188a5357680d08de8ce1168fc3addadb6b800
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-02-09 16:33:29 -0500

    Extracted the schema types

---
+++ src/lib/server/types.ts
@@ -0,0 +1,31 @@
+import { Weekday, type Schedule, type User } from "$lib/Types";
+import { Dayjs } from "dayjs";
+import mongoose, { Schema } from "mongoose";
+
+export type ServerUser = User & { passwordHash: string };
+
+export const UserSchema = new Schema<ServerUser>({
+    da: String,
+    passwordHash: String,
+    email: String,
+    firstName: String,
+    lastName: String,
+    scheduleID: mongoose.Types.ObjectId,
+});
+
+export const ScheduleSchema = new Schema<Schedule>({
+    periods: [
+        {
+            id: String,
+            name: String,
+            group: Number,
+            local: String,
+            type: ["T", "L"],
+            teacher: String,
+            virtual: Boolean,
+            weekday: Weekday,
+            timeStart: Dayjs,
+            timeEnd: Dayjs,
+        },
+    ],
+});
