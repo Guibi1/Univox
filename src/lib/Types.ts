10f51c7b038462261dca6c73cc9ced1f2175e76b
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-02-09 17:35:08 -0500

    Trying to put some logic in the schedule affichage

--- src/lib/Types.ts
+++ src/lib/Types.ts
@@ -29,4 +29,6 @@ export enum Weekday {
     Wednesday = 2,
     Thursday = 3,
     Friday = 4,
+    Saturday = 5,
+    Sunday = 6
 }
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -33,20 +33,45 @@
             timeEnd: dayjs().hour(14),
         },
     ];
+
+    let timeParsing : number = 10;
+
+    const dayOfWeek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+    let matrixSchedule : Class[] = [];
+
+    for (let day in Weekday) {
+        for (let periodNumber in schedule) {
+            let timePeriodStartParsed = schedule[periodNumber].timeStart.hour() * 60 / timeParsing;
+            let timePeriodEndParsed = schedule[periodNumber].timeEnd.hour() * 60 / timeParsing;
+            // for each periods in the day, if the period is within the parsed time periods, put the period in there
+            for (let i = 0; i < 24*60; i += timeParsing) {
+                if (timePeriodStartParsed >= i && i >= timePeriodEndParsed) {
+                    matrixSchedule[i/timeParsing] = schedule[periodNumber];
+                }
+            }
+        }
+    }
+    //! fill somwhere the empty periods with something so html is still created but with no text
+
+
 </script>
 
 <body>
     <table class="w-3/5 border-2">
         <tr>
-            <th>Lundi</th>
-            <th>Mardi</th>
-            <th>Mercredi</th>
-            <th>Jeudi</th>
-            <th>Vendredi</th>
-            <th>Samedi</th>
-            <th>Dimanche</th>
-        </tr>
-        <tr>
+            {#each dayOfWeek as day}
+                <th>
+                    {day}
+                </th> 
+
+                    {#each matrixSchedule as period}
+                        <!-- Do something -->
+                    {/each}
+            {/each}
+        </tr>   
+
+       <tr> 
             <td>Alfreds Futterkiste</td>
             <td>Maria Anders</td>
             <td>Germany</td>
@@ -66,7 +91,7 @@
             <td>Mexico</td>
         </tr>
         <tr>
-            <td></td>
+            <td>{schedule[0].name}</td>
             <td>Francisco Chang</td>
             <td>Mexico</td>
         </tr>
