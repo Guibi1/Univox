33997ba7c1424a1ee9f99cf779e186f0fc9a6221
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-02-09 15:46:09 -0500

    Voir #5 pour une explication

--- svelte.config.js
+++ svelte.config.js
@@ -6,11 +6,7 @@ const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
     preprocess: vitePreprocess(),
-    kit: {
-        adapter: adapter({
-            edge: true,
-        }),
-    },
+    kit: { adapter: adapter() },
 };
 
 export default config;
