3dc299ea497ff977ea997e90ec1616ea57c8aad9
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-02-10 09:09:36 -0500

    Continued the schedule, reworked how it worked

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,23 +1,8 @@
 <script lang="ts">
     import { Weekday, type Class } from "$lib/Types";
+    import ScheduleView from "$lib/components/ScheduleView.svelte";
+    // import user from "$lib/stores/user";
     import dayjs from "dayjs";
-
-    /*
-    this is just for reference, please don't delete
-
-    export type Class = {
-    id: string;
-    name: string;
-    group: number;
-    local: string;
-    type: "T" | "L";
-    teacher: string;
-    virtual: boolean;
-    weekday: Weekday;
-    timeStart: Dayjs;
-    timeEnd: Dayjs;
-    };
-    */
 
     let schedule: Class[] = [
         {
@@ -29,97 +14,10 @@
             teacher: "asdjkfwlbk",
             virtual: false,
             weekday: Weekday.Friday,
-            timeStart: dayjs().hour(12),
+            timeStart: dayjs().hour(9).minute(8),
             timeEnd: dayjs().hour(14),
         },
     ];
-
-    let timeParsing : number = 10;
-
-    const dayOfWeek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
-
-    let matrixSchedule : Class[] = [];
-
-    for (let day in Weekday) {
-        for (let periodNumber in schedule) {
-            let timePeriodStartParsed = schedule[periodNumber].timeStart.hour() * 60 / timeParsing;
-            let timePeriodEndParsed = schedule[periodNumber].timeEnd.hour() * 60 / timeParsing;
-            // for each periods in the day, if the period is within the parsed time periods, put the period in there
-            for (let i = 0; i < 24*60; i += timeParsing) {
-                if (timePeriodStartParsed >= i && i >= timePeriodEndParsed) {
-                    matrixSchedule[i/timeParsing] = schedule[periodNumber];
-                }
-            }
-        }
-    }
-    //! fill somwhere the empty periods with something so html is still created but with no text
-
-
 </script>
 
-<body>
-    <table class="w-3/5 border-2">
-        <tr>
-            {#each dayOfWeek as day}
-                <th>
-                    {day}
-                </th> 
-
-                    {#each matrixSchedule as period}
-                        <!-- Do something -->
-                    {/each}
-            {/each}
-        </tr>   
-
-       <tr> 
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-            <td>Alfreds Futterkiste</td>
-        </tr>   
-        <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-        </tr>
-        <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-        </tr>
-        <tr>
-            <td>{schedule[0].name}</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-        </tr>
-        <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-        </tr>
-        <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-        </tr>
-    </table>
-</body>
-
-<style>
-    tr {
-        text-align: center;
-        height: 50px;
-    }
-
-    td {
-        border-width: 2px;
-    }
-
-    th {
-        border-width: 2px;
-        width: 10rem;
-    }
-</style>
+<ScheduleView {schedule} />
