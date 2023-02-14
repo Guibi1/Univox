5bfaa121d14b2d1d9638cc968a75f5cee3d644b0
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-02-14 00:24:05 -0500

    Logic now matches the other auth pages

--- src/routes/(auth)/mot-de-passe-oublié/+page.svelte
+++ src/routes/(auth)/mot-de-passe-oublié/+page.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
     import { enhance, type SubmitFunction } from "$app/forms";
-    import { goto } from "$app/navigation";
     import user from "$lib/stores/user";
     import type { ActionData } from "./$types";
 
     export let form: ActionData;
     let loading = false;
 
-    $: if (form?.success) {
-        goto("/connexion");
-        user.refresh();
-    }
-
     const handleSubmit = (() => {
         loading = true;
-        return async ({ update }) => {
+        return async ({ result, update }) => {
+            if (result.type === "redirect") {
+                user.refresh();
+            }
             loading = false;
             update();
         };
@@ -23,39 +20,44 @@
 
 <h1 class="text-center">Mot de passe oublié</h1>
 
-<span hidden={form == null || loading} class="text-center text-red-500">
-    {#if form?.missing}
-        Veuillez remplir tous les champs
-    {:else if !form?.incorrect}
-        Vos identifiants Omnivox ne sont pas valide
-    {:else if !form?.noUser}
-        Aucun utilisateur ne correspond au DA entré
-    {/if}
-</span>
-
 {#if loading}
     <box-icon name="loader-circle" animation="spin" class="h-10 my-6 flex items-center w-full" />
 {/if}
 
-<form
-    use:enhance={handleSubmit}
-    hidden={loading}
-    class="flex flex-col justify-between gap-6"
-    method="post"
->
-    <label>
+<form use:enhance={handleSubmit} hidden={loading} class="flex flex-col gap-6" method="post">
+    <label data-error={form?.incorrect}>
         No de DA
-        <input value={form?.da ?? ""} name="da" type="text" />
+        <input
+            name="da"
+            type="text"
+            pattern={"\\d{7}"}
+            required
+            placeholder=" "
+            on:input={() => form && (form.incorrect = false)}
+            value={form?.da ?? ""}
+        />
+        {#if form?.incorrect}
+            <span>Aucun utilisateur ne correspond au DA entré</span>
+        {/if}
     </label>
 
-    <label>
+    <label data-error={form?.omnivoxIncorrect}>
         Mot de passe Omnivox
-        <input name="password" type="password" />
+        <input
+            name="omnivoxPassword"
+            type="password"
+            required
+            placeholder=" "
+            on:input={() => form && (form.omnivoxIncorrect = false)}
+        />
+        {#if form?.omnivoxIncorrect}
+            <span>Mot de passe Omnivox erroné</span>
+        {/if}
     </label>
 
     <label>
         Nouveau mot de passe
-        <input name="newPassword" type="password" />
+        <input name="newPassword" type="password" pattern={".{8,}"} required placeholder=" " />
     </label>
 
     <a href="/connexion" class="flex self-start">
