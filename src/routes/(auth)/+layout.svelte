3a6b4a1c672de7bc165303bd8141b9e5c6b45d3a
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-02-14 01:23:32 -0500

    Redid the auth style to be better ?

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,14 +1,6 @@
-<script lang="ts">
-    import logo from "$assets/logo.webp";
-</script>
+<div class="min-h-screen flex justify-center items-center bg-stone-200">
+    <main class="bg-white py-8 px-10 rounded w-[32rem] h-max flex flex-col gap-2 items-stretch">
 
-<div class="min-h-screen flex justify-evenly items-center bg-stone-200">
-    <div class="flex flex-col gap-4 items-center">
-        <img src={logo} alt="Univox's logo" />
-        <h1 class="font-bold text-4xl items-center ">Univox</h1>
-    </div>
-
-    <main class="bg-white p-6 px-10 rounded w-96 h-max">
         <slot />
     </main>
 </div>
--- src/routes/(auth)/mot-de-passe-oublié/+page.svelte
+++ src/routes/(auth)/mot-de-passe-oublié/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { enhance, type SubmitFunction } from "$app/forms";
+    import logo from "$assets/logo.webp";
     import user from "$lib/stores/user";
     import type { ActionData } from "./$types";
 
@@ -18,52 +19,64 @@
     }) satisfies SubmitFunction;
 </script>
 
-<h1 class="text-center">Mot de passe oublié</h1>
+<div class="pb-4 flex items-center justify-center gap-4">
+    <img src={logo} class="h-12 aspect-square" alt="Univox's logo" />
+    <h1>Mot de passe oublié</h1>
+</div>
 
 {#if loading}
     <box-icon name="loader-circle" animation="spin" class="h-10 my-6 flex items-center w-full" />
 {/if}
 
-<form use:enhance={handleSubmit} hidden={loading} class="flex flex-col gap-6" method="post">
-    <label data-error={form?.incorrect}>
-        No de DA
-        <input
-            name="da"
-            type="text"
-            pattern={"\\d{7}"}
-            required
-            placeholder=" "
-            on:input={() => form && (form.incorrect = false)}
-            value={form?.da ?? ""}
-        />
-        {#if form?.incorrect}
-            <span>Aucun utilisateur ne correspond au DA entré</span>
-        {/if}
-    </label>
+<form
+    use:enhance={handleSubmit}
+    hidden={loading}
+    class="m-auto w-9/12 flex flex-col gap-6"
+    method="post"
+>
+    <div class="flex flex-col gap-4">
+        <label data-error={form?.incorrect}>
+            No de DA
+            <input
+                name="da"
+                type="text"
+                pattern={"\\d{7}"}
+                required
+                placeholder=" "
+                on:input={() => form && (form.incorrect = false)}
+                value={form?.da ?? ""}
+            />
+            {#if form?.incorrect}
+                <span>Aucun utilisateur ne correspond au DA entré</span>
+            {/if}
+        </label>
 
-    <label data-error={form?.omnivoxIncorrect}>
-        Mot de passe Omnivox
-        <input
-            name="omnivoxPassword"
-            type="password"
-            required
-            placeholder=" "
-            on:input={() => form && (form.omnivoxIncorrect = false)}
-        />
-        {#if form?.omnivoxIncorrect}
-            <span>Mot de passe Omnivox erroné</span>
-        {/if}
-    </label>
+        <label data-error={form?.omnivoxIncorrect}>
+            Mot de passe Omnivox
+            <input
+                name="omnivoxPassword"
+                type="password"
+                required
+                placeholder=" "
+                on:input={() => form && (form.omnivoxIncorrect = false)}
+            />
+            {#if form?.omnivoxIncorrect}
+                <span>Mot de passe Omnivox erroné</span>
+            {/if}
+        </label>
 
-    <label>
-        Nouveau mot de passe
-        <input name="newPassword" type="password" pattern={".{8,}"} required placeholder=" " />
-    </label>
+        <label>
+            Nouveau mot de passe
+            <input name="newPassword" type="password" pattern={".{8,}"} required placeholder=" " />
+        </label>
 
-    <a href="/connexion" class="flex self-start">
-        <box-icon name="chevron-left" class="fill-cyan-300" />
-        Retour
-    </a>
+        <a href="/connexion" class="flex self-start">
+            <box-icon name="chevron-left" class="fill-cyan-300" />
+            Retour
+        </a>
+    </div>
 
-    <button type="submit">Suivant</button>
+    <button type="submit" class="w-7/12 self-center flex items-center justify-center">
+        Suivant <box-icon name="chevron-right" />
+    </button>
 </form>
