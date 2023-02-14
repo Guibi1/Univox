8986e1a8a70b3d70f3d917430167bb688ee267c3
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-02-13 19:31:39 -0500

    Début de validation des forms

--- src/routes/(auth)/inscription/+page.server.ts
+++ src/routes/(auth)/inscription/+page.server.ts
@@ -4,30 +4,51 @@ import { fail } from "@sveltejs/kit";
 import bcryptjs from "bcryptjs";
 import mongoose from "mongoose";
 import type { Actions } from "./$types";
+import validator from "validator";
 
 export const actions = {
     default: async ({ request }) => {
         const data = await request.formData();
-        const firstName = data.get("firstName");
-        const lastName = data.get("lastName");
+        const firstStep = data.get("firstStep")?.valueOf() == "true";
         const da = data.get("da");
         const omnivoxPassword = data.get("omnivoxPassword");
+
+        if (!da || !omnivoxPassword) {
+            return fail(400, { da, missing: true });
+        }
+
+        if (firstStep) {
+            try {
+                await login(da.toString(), omnivoxPassword.toString());
+            } catch (e) {
+                return fail(401, { da, omnivoxIncorrect: true });
+            }
+
+            if (await db.findUser({ da })) {
+                return fail(400, { da, daExists: true });
+            }
+
+            return { success: true };
+        }
+
+        const firstName = data.get("firstName");
+        const lastName = data.get("lastName");
         const email = data.get("email");
         const password = data.get("password");
 
-        if (!firstName || !lastName || !email || !da || !password || !omnivoxPassword) {
-            return fail(400, { da, firstName, lastName, email, missing: true });
-        } else if (!email.toString()) {
-            // TODO: install yum and check the inputs
-            return fail(400, { da, firstName, lastName, email, emailIncorrect: true });
+        if (!firstName || !lastName || !email || !password) {
+            return fail(400, { da, omnivoxPassword, firstName, lastName, email, missing: true });
+        } else if (!validator.isEmail(email.toString())) {
+            return fail(400, {
+                da,
+                omnivoxPassword,
+                firstName,
+                lastName,
+                email,
+                emailIncorrect: true,
+            });
         } else if (await db.findUser({ da })) {
-            return fail(400, { da, firstName, lastName, email, daExists: true });
-        }
-
-        try {
-            await login(da.toString(), omnivoxPassword.toString());
-        } catch (e) {
-            return fail(401, { da, firstName, lastName, email, omnivoxIncorrect: true });
+            return fail(400, { da, omnivoxPassword, firstName, lastName, email, daExists: true });
         }
 
         await db.createUser({
