fe18984130c3c2b5d0f10498490fd07e426f12a0
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-02-14 00:24:05 -0500

    Logic now matches the other auth pages

--- src/routes/(auth)/mot-de-passe-oublié/+page.server.ts
+++ src/routes/(auth)/mot-de-passe-oublié/+page.server.ts
@@ -6,28 +6,33 @@ import type { Actions } from "./$types";
 export const actions = {
     default: async ({ request }) => {
         const data = await request.formData();
-        const da = data.get("da");
-        const password = data.get("password");
+        const da = data.get("da")?.toString();
+        const omnivoxPassword = data.get("omnivoxPassword")?.toString();
         const newPassword = data.get("newPassword")?.toString();
 
-        if (!da || !password || !newPassword) {
+        if (
+            !da ||
+            !/\d{7}/.test(da) ||
+            !omnivoxPassword ||
+            !newPassword ||
+            !/.{8,}/.test(newPassword)
+        ) {
             return fail(400, { da, missing: true });
         }
 
         try {
-            await login(da.toString(), password.toString());
+            await login(da, omnivoxPassword);
         } catch (e) {
-            return fail(401, { da, incorrect: true });
+            return fail(401, { da, omnivoxIncorrect: true });
         }
 
         const user = await db.findUser({ da });
         if (!user) {
-            // ? Maybe not fail so that the users can't find all the DA that are registered
-            return fail(401, { da, noUser: true });
+            return fail(401, { da, incorrect: true });
         }
 
-        await db.updateUserPassword(user?._id, newPassword.toString());
+        await db.updateUserPassword(user?._id, newPassword);
 
-        return { success: true };
+        throw redirect(302, "/connexion");
     },
 } satisfies Actions;
