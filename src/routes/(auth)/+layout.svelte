3d4a4dcf74a2d1dd902681d5556f63cf2e9850de
Virliott <123177639+Virliott@users.noreply.github.com>
2023-02-17 10:02:20 -0500

    Update +layout.svelte

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,6 +1,5 @@
 <div class="min-h-screen flex justify-center items-center bg-stone-200">
-    <main class="bg-white py-8 px-10 rounded w-[32rem] h-max flex flex-col gap-2 items-stretch">
-
+    <main class="py-8 px-10 w-[32rem] h-max rounded flex flex-col gap-2 items-stretch bg-white dark:bg-neutral-800">
         <slot />
     </main>
 </div>
