2c8bc04fe1ff1ce2e835150ea2276453ddafbfab
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-02-22 11:43:23 -0500

    Ajout de transitions :)

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,5 +1,17 @@
+<script lang="ts">
+    let height: number | null;
+</script>
+
 <div class="min-h-screen flex justify-center items-center bg-stone-200">
-    <main class="py-8 px-10 w-[32rem] h-max rounded flex flex-col gap-2 items-stretch bg-white dark:bg-neutral-800">
-        <slot />
-    </main>
+    <div
+        class="relative w-[32rem] overflow-hidden rounded bg-white dark:bg-neutral-800 transition-[height]"
+        style="height: {height}px;"
+    >
+        <main
+            bind:clientHeight={height}
+            class="absolute py-8 px-10 w-[32rem] flex flex-col gap-2 items-stretch"
+        >
+            <slot />
+        </main>
+    </div>
 </div>
--- src/routes/(auth)/inscription/+page.svelte
+++ src/routes/(auth)/inscription/+page.svelte
@@ -24,111 +24,121 @@
     <h1>Inscription</h1>
 </div>
 
-{#if loading}
-    <box-icon name="loader-circle" animation="spin" class="h-10 my-6 flex items-center w-full" />
-{/if}
-
-<form
-    use:enhance={handleSubmit}
-    hidden={loading}
-    class="m-auto w-9/12 flex flex-col gap-6"
-    method="post"
->
+<form use:enhance={handleSubmit} class="m-auto w-9/12 flex flex-col gap-6" method="post">
     <input hidden name="firstStep" value={firstStep} />
 
-    <div hidden={!firstStep} class="grid gap-4">
-        <label data-error={form?.daExists}>
-            No de DA
-            <input
-                name="da"
-                type="text"
-                pattern={"\\d{7}"}
-                required={firstStep}
-                placeholder=" "
-                on:input={() => form && (form.daExists = false)}
-                value={form?.da ?? ""}
-            />
-            {#if form?.daExists}
-                <span>Un compte avec ce DA existe déjà</span>
-            {/if}
-        </label>
+    <div
+        class="relative w-[240%] grid grid-cols-2 gap-[20%] transition-[right]"
+        style="right: {firstStep ? '0' : '140'}%;"
+    >
+        <div class="grid gap-4">
+            <label data-error={form?.daExists}>
+                No de DA
+                <input
+                    name="da"
+                    type="text"
+                    pattern={"\\d{7}"}
+                    required={firstStep}
+                    placeholder=" "
+                    on:input={() => form && (form.daExists = false)}
+                    value={form?.da ?? ""}
+                    readonly={loading}
+                />
+                {#if form?.daExists}
+                    <span>Un compte avec ce DA existe déjà</span>
+                {/if}
+            </label>
 
-        <label data-error={form?.omnivoxIncorrect}>
-            Mot de passe Omnivox
-            <input
-                name="omnivoxPassword"
-                type="password"
-                required={firstStep}
-                placeholder=" "
-                on:input={() => form && (form.omnivoxIncorrect = false)}
-            />
-            {#if form?.omnivoxIncorrect}
-                <span>Mot de passe erroné</span>
-            {/if}
-        </label>
+            <label data-error={form?.omnivoxIncorrect}>
+                Mot de passe Omnivox
+                <input
+                    name="omnivoxPassword"
+                    type="password"
+                    required={firstStep}
+                    placeholder=" "
+                    on:input={() => form && (form.omnivoxIncorrect = false)}
+                    readonly={loading}
+                />
+                {#if form?.omnivoxIncorrect}
+                    <span>Mot de passe erroné</span>
+                {/if}
+            </label>
+        </div>
+
+        <div hidden={firstStep} class="grid grid-cols-2 gap-4">
+            <label>
+                Prénom
+                <input
+                    name="firstName"
+                    type="text"
+                    pattern={"\\D{2,}"}
+                    required={!firstStep}
+                    placeholder=" "
+                    value={form?.firstName ?? ""}
+                    readonly={loading}
+                />
+            </label>
+
+            <label>
+                Nom
+                <input
+                    name="lastName"
+                    type="text"
+                    pattern={"\\D{2,}"}
+                    required={!firstStep}
+                    placeholder=" "
+                    value={form?.lastName ?? ""}
+                    readonly={loading}
+                />
+            </label>
+
+            <label class="col-span-2">
+                Courriel
+                <input
+                    name="email"
+                    type="email"
+                    pattern={"[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+(\\.[a-zA-Z]+)+"}
+                    required={!firstStep}
+                    placeholder=" "
+                    value={form?.email ?? ""}
+                    readonly={loading}
+                />
+            </label>
+
+            <label class="col-span-2">
+                Mot de passe
+                <input
+                    name="password"
+                    type="password"
+                    pattern={".{8,}"}
+                    required={!firstStep}
+                    placeholder=" "
+                    readonly={loading}
+                />
+            </label>
+
+            <!-- svelte-ignore a11y-invalid-attribute -->
+            <a
+                on:click={() => (firstStep = true)}
+                class="col-span-2 self-left flex items-center"
+                href="javascript:void()"
+            >
+                <box-icon name="chevron-left" class="fill-cyan-300" /> Retour
+            </a>
+        </div>
     </div>
 
-    <div hidden={firstStep} class="grid grid-cols-2 gap-4">
-        <label>
-            Prénom
-            <input
-                name="firstName"
-                type="text"
-                pattern={"\\D{2,}"}
-                required={!firstStep}
-                placeholder=" "
-                value={form?.firstName ?? ""}
-            />
-        </label>
-
-        <label>
-            Nom
-            <input
-                name="lastName"
-                type="text"
-                pattern={"\\D{2,}"}
-                required={!firstStep}
-                placeholder=" "
-                value={form?.lastName ?? ""}
-            />
-        </label>
-
-        <label class="col-span-2">
-            Courriel
-            <input
-                name="email"
-                type="email"
-                pattern={"[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+(\\.[a-zA-Z]+)+"}
-                required={!firstStep}
-                placeholder=" "
-                value={form?.email ?? ""}
-            />
-        </label>
-
-        <label class="col-span-2">
-            Mot de passe
-            <input
-                name="password"
-                type="password"
-                pattern={".{8,}"}
-                required={!firstStep}
-                placeholder=" "
-            />
-        </label>
-
-        <!-- svelte-ignore a11y-invalid-attribute -->
-        <a
-            on:click={() => (firstStep = true)}
-            class="col-span-2 self-left flex items-center"
-            href="javascript:void()"
-        >
-            <box-icon name="chevron-left" class="fill-cyan-300" /> Retour
-        </a>
-    </div>
-
-    <button type="submit" class="w-7/12 self-center flex items-center justify-center">
-        {firstStep ? "Suivant" : "S'inscrire"} <box-icon name="chevron-right" />
-    </button>
+    {#if !loading}
+        <button type="submit" class="w-7/12 self-center flex items-center justify-center">
+            {firstStep ? "Suivant" : "S'inscrire"} <box-icon name="chevron-right" />
+        </button>
+    {:else}
+        <box-icon
+            name="loader-circle"
+            animation="spin"
+            class="h-10 my-6 flex items-center w-full"
+        />
+    {/if}
 </form>
 
 {#if !loading}
