04a4087e419a3a570ef41a8ab1cfc328ab3d8743
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-02-22 18:58:02 -0500

    Added a wave background

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
+    import WavesBackground from "./WavesBackground.svelte";
+
     let height: number | null;
 </script>
 
-<div class="min-h-screen flex justify-center items-center bg-stone-200">
+<WavesBackground />
+
+<div class="h-screen flex justify-center items-center">
     <div
-        class="relative w-[32rem] overflow-hidden rounded bg-white dark:bg-neutral-800 transition-[height]"
+        class="relative w-[32rem] overflow-hidden rounded bg-white dark:bg-neutral-800 !bg-opacity-[85%] backdrop-blur transition-[height]"
         style="height: {height}px;"
     >
         <main
             bind:clientHeight={height}
-            class="absolute py-8 px-10 w-[32rem] flex flex-col gap-2 items-stretch"
+            class="absolute w-full py-8 px-10 flex flex-col gap-2 items-stretch"
         >
             <slot />
         </main>
