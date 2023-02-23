28739dda0a1aaf81fc97b99bf86da7f6b59fd82a
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-02-23 11:51:39 -0500

    Horaire file
    Horaire can now accept diffferent dates
    
    Horaire is now commented!

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,31 +1,29 @@
 <script lang="ts">
-    import { Weekday, type Class } from "$lib/Types";
+    import type { Class } from "$lib/Types";
     import ScheduleView from "$lib/components/ScheduleView.svelte";
-    // import user from "$lib/stores/user";
     import dayjs from "dayjs";
+    import mongoose from "mongoose";
 
     let schedule: Class[] = [
         {
-            id: "theIDLOL",
+            _id: new mongoose.Types.ObjectId(),
             name: "aName",
             group: 105,
             local: "B-105",
             type: "L",
             teacher: "asdjkfwlbk",
             virtual: false,
-            weekday: Weekday.Friday,
             timeStart: dayjs().hour(6).minute(8),
             timeEnd: dayjs().hour(8).minute(0),
         },
         {
-            id: "theIDLOL",
+            _id: new mongoose.Types.ObjectId(),
             name: "aName",
             group: 105,
             local: "B-105",
             type: "L",
             teacher: "asdjkfwlbk",
             virtual: false,
-            weekday: Weekday.Wednesday,
             timeStart: dayjs().hour(14).minute(30),
             timeEnd: dayjs().hour(16).minute(0),
         },
