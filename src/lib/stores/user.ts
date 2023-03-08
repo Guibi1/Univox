8de3a13820c1a4d578da36dec5f1cf55371e12d2
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-03-08 18:02:39 -0500

    Syncronisation du user entre les onglets

--- src/lib/stores/user.ts
+++ src/lib/stores/user.ts
@@ -1,8 +1,22 @@
 import type { User } from "$lib/Types";
+import { onMount } from "svelte";
 import { writable } from "svelte/store";
 
 function createUserStore() {
-    const { subscribe, set } = writable<User | null>();
+    const { subscribe, set: setStore } = writable<User | null>();
+    let bc: BroadcastChannel;
+
+    // This syncs the different tabs' user
+    onMount(() => {
+        bc = new BroadcastChannel("New user data");
+        bc.addEventListener("message", (e) => setStore(e.data));
+        return () => bc.close();
+    });
+
+    const set: typeof setStore = (user) => {
+        setStore(user);
+        if (bc) bc.postMessage(user);
+    };
 
     async function refresh() {
         const user = await (await fetch("/api/user")).json();
