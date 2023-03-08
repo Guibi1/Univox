b8f154c0b0a2657168c5732566453d10b853f81d
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-03-08 17:47:47 -0500

    Les onglet se communiquent pour le login

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+    import { invalidate } from "$app/navigation";
+    import { onMount } from "svelte";
     import Navbar from "./Navbar.svelte";
+
+    onMount(() => {
+        const bc = new BroadcastChannel("Invalidate user");
+        bc.postMessage("");
+        bc.addEventListener("message", () => invalidate("app:user"));
+        return () => bc.close();
+    });
 </script>
 
 <Navbar />
--- src/routes/(auth)/+layout.server.ts
+++ src/routes/(auth)/+layout.server.ts
@@ -1,7 +1,8 @@
 import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
-export const load = (({ locals, url }) => {
+export const load = (({ locals, url, depends }) => {
+    depends("app:user");
     const ref = url.searchParams.get("ref");
     if (!!locals.user && url.pathname !== "/deconnexion") {
         throw redirect(307, ref ?? "/");
