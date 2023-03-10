148ae4fc096c9ee651cbbdb09a777a35a5c57817
alexandreVeregut <123177507+alexandreVeregut@users.noreply.github.com>
2023-03-10 11:35:36 -0500

    Started UI add books

--- src/routes/(app)/livres/nouveau-livre/+page@(app).svelte
+++ src/routes/(app)/livres/nouveau-livre/+page@(app).svelte
@@ -13,13 +13,12 @@
     <title>Univox | Livres | Nouveau livre</title>
 </svelte:head>
 
-<div class="min-h-screen flex justify-evenly items-center bg-white dark:bg-neutral-900">
-    <main class="bg-neutral-900 dark:bg-white p-12 rounded w-96 h-max">
-        <form
-            on:submit={() => doSomething(titreLivre, etatLivre)}
-            class="flex flex-col justify-between gap-6"
-        >
-            <h1 class="text-center">Vendre un livre</h1>
+<form
+    on:submit={() => doSomething(titreLivre, nomAuteur, etatLivre, prix)}
+    class="grid grid-cols-[1fr_2fr] gap-6"
+>
+    <div class="flex flex-col items-stretch gap-5">
+        <h1 class="text-center">Vendre un livre</h1>
 
         <label>
             Nom du livre
@@ -40,17 +39,10 @@
             <input bind:value={prix} required />
         </label>
     </div>
-
-    <div class="flex flex-col items-stretch gap-5 pr-40 pt-20">
+    <div class="pt-20">
         <div
-            class="box-border h-[250px] rounded-xl border-4 border-gray-600 bg-gray-600 bg-opacity-60"
+            class="box-border h-2/3 border-4 radius-10 border-gray-600 bg-gray-600 bg-opacity-60"
         />
-
-        <button
-            class="hover:bg-blue-secondary active:bg-cyan-900 focus:ring focus:ring-gray-500"
-            type="submit"
-        >
-            Créer l'annonce
-        </button>
+        <button class="w-2/3" type="submit">Créer l'annonce</button>
     </div>
 </form>
