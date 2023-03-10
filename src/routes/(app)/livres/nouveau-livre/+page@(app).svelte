cb37983e26b4132f48de5178e6e73d0f48567223
alexandreVeregut <123177507+alexandreVeregut@users.noreply.github.com>
2023-03-10 11:35:36 -0500

    Started UI add books

--- src/routes/(app)/livres/nouveau-livre/+page@(app).svelte
+++ src/routes/(app)/livres/nouveau-livre/+page@(app).svelte
@@ -1,40 +1,48 @@
 <script lang="ts">
     let titreLivre = "";
+    let nomAuteur = "";
     let etatLivre = "";
     let prix: string = "";
 
-    function doSomething(titreLivre: any, etatLivre: any): any {
+    function doSomething(titreLivre: any, nomAuteur: any, etatLivre: any, prix: any): any {
         throw new Error("Function not implemented.");
     }
 </script>
 
 <svelte:head>
-    <title> Univox | Livres | Nouveau livre </title>
+    <title>Univox | Livres | Nouveau livre</title>
 </svelte:head>
 
-<div class="min-h-screen flex justify-evenly items-center bg-white dark:bg-neutral-900">
-    <main class="bg-neutral-900 dark:bg-white p-12 rounded w-96 h-max">
-        <form
-            on:submit={() => doSomething(titreLivre, etatLivre)}
-            class="flex flex-col justify-between gap-6"
-        >
-            <h1 class="text-center">Vendre un livre</h1>
+<form
+    on:submit={() => doSomething(titreLivre, nomAuteur, etatLivre, prix)}
+    class="grid grid-cols-[1fr_2fr] gap-6"
+>
+    <div class="flex flex-col items-stretch gap-5">
+        <h1 class="text-center">Vendre un livre</h1>
 
-            <label>
-                Nom du livre
-                <input bind:value={titreLivre} required />
-            </label>
+        <label>
+            Nom du livre
+            <input bind:value={titreLivre} required />
+        </label>
 
-            <label>
-                L'état du livre
-                <input bind:value={etatLivre} required />
-            </label>
-            <label>
-                Prix de vente
-                <input bind:value={prix} required />
-            </label>
+        <label>
+            Nom de(s) (l')auteur(s)
+            <input bind:value={nomAuteur} required />
+        </label>
 
-            <button type="submit">Créer l'annonce</button>
-        </form>
-    </main>
-</div>
+        <label>
+            L'état du livre
+            <input bind:value={etatLivre} required />
+        </label>
+        <label>
+            Prix de vente
+            <input bind:value={prix} required />
+        </label>
+    </div>
+    <div class="pt-20">
+        <div
+            class="box-border h-2/3 border-4 radius-10 border-gray-600 bg-gray-600 bg-opacity-60"
+        />
+        <button class="w-2/3" type="submit">Créer l'annonce</button>
+    </div>
+</form>
