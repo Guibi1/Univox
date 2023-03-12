1f32a8caa35cd726b815c042db517c91b7caaa67
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-03-12 16:04:29 -0400

    Debug again

--- checks/auth.check.js
+++ checks/auth.check.js
@@ -15,8 +15,8 @@ const AuthCheck = () => {
         // Click on <button> "Se connecter"
         await page.click('[type="submit"]');
 
-        await expect(page).toHaveURL(univox.getAbsoluteURI("/connexion"));
         await page.waitForTimeout(1000);
+        await expect(page).toHaveURL(univox.getAbsoluteURI("/connexion"));
         await expect(page.locator("text=Mot de passe erroné")).toBeVisible();
     });
 
@@ -33,7 +33,7 @@ const AuthCheck = () => {
         // Click on <button> "Se connecter"
         await page.click('[type="submit"]');
 
-        expect(page.url()).toMatch(univox.getAbsoluteURI("/"));
+        await expect(page).toHaveURL(univox.getAbsoluteURI("/"));
     });
 };
 
