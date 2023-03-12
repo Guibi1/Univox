338bf5b6e78a37219044390acad5478193d5c24a
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-03-12 17:56:47 -0400

    Added book checks

--- checks/auth.check.js
+++ checks/auth.check.js
@@ -7,13 +7,13 @@ const AuthCheck = () => {
         await univox.goto("/connexion");
 
         // Fill "da" on <input> [name="da"]
-        await page.fill('[name="da"]', "0000000");
+        await page.fill('input[name="da"]', "0000000");
 
         // Fill "password" on <input> [name="password"]
-        await page.fill('[name="password"]', "00000000");
+        await page.fill('input[name="password"]', "00000000");
 
         // Click on <button> "Se connecter"
-        await page.click('[type="submit"]');
+        await page.click('button[type="submit"]');
 
         await expect(page).toHaveURL(univox.getAbsoluteURI("/connexion"));
         await expect(page.locator("text=Mot de passe erroné")).toBeVisible();
@@ -24,17 +24,17 @@ const AuthCheck = () => {
         await univox.goto("/connexion");
 
         // Fill "da" on <input> [name="da"]
-        await page.fill('[name="da"]', process.env.DA);
+        await page.fill('input[name="da"]', process.env.DA);
 
         // Fill "password" on <input> [name="password"]
-        await page.fill('[name="password"]', process.env.PASSWORD);
+        await page.fill('input[name="password"]', process.env.PASSWORD);
 
         // Click on <button> "Se connecter"
-        await page.click('[type="submit"]');
+        await page.click('button[type="submit"]');
         await expect(page).toHaveURL(univox.getAbsoluteURI("/"));
 
         // Click on <a> "Déconnexion"
-        await page.click('[href="/deconnexion"]');
+        await page.click('a[href="/deconnexion"]');
         await expect(page).toHaveURL(univox.getAbsoluteURI("/connexion"));
     });
 };
