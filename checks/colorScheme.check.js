0429a8700a7136d7dd8cc8139a7fd1b25a3e8abc
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-03-13 09:48:31 -0400

    Shortened color scheme check

--- checks/colorScheme.check.js
+++ checks/colorScheme.check.js
@@ -2,37 +2,22 @@ import { expect, test } from "@playwright/test";
 import { UnivoxPage } from "./univoxPage";
 
 const ColorSchemeCheck = () => {
-    test("Dark mode is enabled by default", async ({ page }) => {
+    test("Color scheme btuuon works and is consistent between refreshes", async ({ page }) => {
         const univox = new UnivoxPage(page);
         await univox.goto("/connexion");
 
         await expect(page.locator("html")).toHaveAttribute("data-colorscheme", "dark");
-    });
-
-    test("Color scheme button switches to light mode and back", async ({ page }) => {
-        const univox = new UnivoxPage(page);
-        await univox.goto("/connexion");
-
-        // Click on <button> [aria-label="thème du site"]
-        await page.click('[aria-label="thème du site"]');
-        await expect(page.locator("html")).toHaveAttribute("data-colorscheme", "light");
-
-        // Click on <button> [aria-label="thème du site"]
-        await page.click('[aria-label="thème du site"]');
-        await expect(page.locator("html")).toHaveAttribute("data-colorscheme", "dark");
-    });
-
-    test("Color scheme is consistent across refresh", async ({ page }) => {
-        const univox = new UnivoxPage(page);
-        await univox.goto("/connexion");
 
         // Click on <button> [aria-label="thème du site"]
         await page.click('[aria-label="thème du site"]');
         await expect(page.locator("html")).toHaveAttribute("data-colorscheme", "light");
 
         await page.reload();
-
         await expect(page.locator("html")).toHaveAttribute("data-colorscheme", "light");
+
+        // Click on <button> [aria-label="thème du site"]
+        await page.click('[aria-label="thème du site"]');
+        await expect(page.locator("html")).toHaveAttribute("data-colorscheme", "dark");
     });
 };
 
