cd5cdd93e5e65961c778bafffa61aa77b29662e4
Laurent Stéphenne <guibi.ss999@gmail.com>
2023-03-13 10:09:08 -0400

    Check fixes

--- checks/univox.check.js
+++ checks/univox.check.js
@@ -10,25 +10,27 @@ import ColorSchemeCheck from "./colorScheme.check";
 test.describe("Auth routes", AuthCheck);
 test.describe("Color Scheme", ColorSchemeCheck);
 
-test.beforeAll(async ({ page }) => {
-    const univox = new UnivoxPage(page);
-    await univox.goto("/connexion");
+test.describe("Logged in", () => {
+    test.beforeAll(async ({ page }) => {
+        const univox = new UnivoxPage(page);
+        await univox.goto("/connexion");
 
-    // Fill "da" on <input> [name="da"]
-    await page.fill('input[name="da"]', process.env.DA);
+        // Fill "da" on <input> [name="da"]
+        await page.fill('input[name="da"]', process.env.DA);
 
-    // Fill "password" on <input> [name="password"]
-    await page.fill('input[name="password"]', process.env.PASSWORD);
+        // Fill "password" on <input> [name="password"]
+        await page.fill('input[name="password"]', process.env.PASSWORD);
 
-    // Click on <button> "Se connecter"
-    await page.click('button[type="submit"]');
-    await expect(page).toHaveURL(univox.getAbsoluteURI("/"));
+        // Click on <button> "Se connecter"
+        await page.click('button[type="submit"]');
+        await expect(page).toHaveURL(univox.getAbsoluteURI("/"));
+    });
+    test.afterAll(async ({ page }) => {
+        const univox = new UnivoxPage(page);
+        await univox.goto("/");
+
+        await page.click('a[href="/deconnexion"]');
+    });
+
+    test.describe("Book routes", BookCheck);
 });
-test.afterAll(async ({ page }) => {
-    const univox = new UnivoxPage(page);
-    await univox.goto("/");
-
-    await page.click('a[href="/deconnexion"]');
-});
-
-test.describe("Book routes", BookCheck);
