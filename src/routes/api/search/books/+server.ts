a8b2ffd88f54b4116c243c06408aa3b1d4bf6491
Laurent Stéphenne <laurent@guibi.ca>
2023-03-20 14:25:41 -0400

    La recherche fonctionne!

--- src/routes/api/search/books/+server.ts
+++ src/routes/api/search/books/+server.ts
@@ -2,11 +2,11 @@ import * as db from "$lib/server/db";
 import { error, json } from "@sveltejs/kit";
 import type { RequestHandler } from "./[query]/$types";
 
-export const GET = (async ({ locals, request }) => {
+export const POST = (async ({ locals, request }) => {
     if (!locals.user) throw error(401);
 
-    const { query, filter } = await request.json();
-    if (typeof query != "string" || !Array.isArray(filter)) throw error(400);
+    const { query, codes } = await request.json();
+    if (typeof query != "string" || !Array.isArray(codes)) throw error(400);
 
-    return json(await db.searchBooks(locals.user, query, filter));
+    return json(await db.searchBooks(locals.user, query, codes));
 }) satisfies RequestHandler;
