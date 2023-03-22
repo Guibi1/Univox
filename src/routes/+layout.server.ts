b1e728da4c4108209e4ef587c36b277fc61488b0
azom <97298292+azomwastaken@users.noreply.github.com>
2023-03-22 15:59:48 -0400

    29 Ajouter des paramètres de l'utilisateur (#32)
    * Page de paramètre créé et ajout d'un cookie
    
    La page est créé avec:
    -Aucun style
    -Une option pour changer le jour de début de semaine
    
    Autre:
    - Ajouter le cookie pour le premier jour
    - Il peut être set dans les paramètres mais il est pas encore lu pour l'horaire
    
    * Updated for the cookie startWeekDate to work (pas dans horaire yet)
    
    * Page de paramètre créé et ajout d'un cookie
    
    La page est créé avec:
    -Aucun style
    -Une option pour changer le jour de début de semaine
    
    Autre:
    - Ajouter le cookie pour le premier jour
    - Il peut être set dans les paramètres mais il est pas encore lu pour l'horaire
    
    * Updated for the cookie startWeekDate to work (pas dans horaire yet)
    
    * Merge branch '29-ajouter-des-parametres-de-lutilisateur' of https://github.com/Guibi1/H23-Gr3-Equipe3-Univox into 29-ajouter-des-parametres-de-lutilisateur
    
    * On peut maintenant changer la date du début de la semaine
    
    * Ajout d'un precommit hook pour prettier
    
    * Formattage de tout le projet
    
    * On utilise .vscode et non les workspaces
    
    * Formaté
    
    * zzzzzzzzzzzzzzzzzz
    
    * Fixed type
    
    * The start week day settings completely works
    
    As i'm aware of...
    
    * Page de paramètre créé et ajout d'un cookie
    
    La page est créé avec:
    -Aucun style
    -Une option pour changer le jour de début de semaine
    
    Autre:
    - Ajouter le cookie pour le premier jour
    - Il peut être set dans les paramètres mais il est pas encore lu pour l'horaire
    
    * Updated for the cookie startWeekDate to work (pas dans horaire yet)
    
    * Page de paramètre créé et ajout d'un cookie
    
    La page est créé avec:
    -Aucun style
    -Une option pour changer le jour de début de semaine
    
    Autre:
    - Ajouter le cookie pour le premier jour
    - Il peut être set dans les paramètres mais il est pas encore lu pour l'horaire
    
    * Updated for the cookie startWeekDate to work (pas dans horaire yet)
    
    * On peut maintenant changer la date du début de la semaine
    
    * The start week day settings completely works
    
    As i'm aware of...
    
    * Merge branch '29-ajouter-des-parametres-de-lutilisateur' of https://github.com/Guibi1/H23-Gr3-Equipe3-Univox into 31-amélioration-UI-Acceuil-Et-Livres
    
    * Fixed some dead code
    
    * I think this is why vercel didin't like it
    
    Changed the path of the colorScheme api to the new one for one line
    
    Co-Authored-By: Laurent Stéphenne <laurent@guibi.ca>

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,14 +1,16 @@
-import type { ColorScheme } from "$lib/stores/colorScheme";
-import colorScheme from "$lib/stores/colorScheme";
+import colorScheme, { type ColorScheme } from "$lib/stores/colorScheme";
 import friends from "$lib/stores/friends";
+import startWeekDate, { type StartWeekDate } from "$lib/stores/startWeekDate";
 import user from "$lib/stores/user";
 import type { LayoutServerLoad } from "./$types";
 
 export const load = (({ cookies, locals }) => {
     const color = (cookies.get("colorScheme") ?? "dark") as ColorScheme;
+    const startDate = (cookies.get("startWeekDate") ?? "Dimanche") as StartWeekDate;
 
     // Stores are up to date during SSR
     colorScheme.setInitial(color);
+    startWeekDate.setInitial(startDate);
     user.set(locals.user);
     friends.set(locals.friends);
 
@@ -16,6 +18,7 @@ export const load = (({ cookies, locals }) => {
     return {
         storesInitialValue: {
             colorScheme: color,
+            startWeekDate: startDate,
             serializedUser: JSON.stringify(locals.user),
             serializedFriends: JSON.stringify(locals.friends),
         },
