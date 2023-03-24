95cf45f4f310733df58db62182ef1cf593783191
Laurent Stéphenne <laurent@guibi.ca>
2023-03-24 00:29:59 -0400

    Now using workbox and it works

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,69 +1,9 @@
-/// <reference types="@sveltejs/kit" />
-/// <reference no-default-lib="true"/>
-/// <reference lib="esnext" />
-/// <reference lib="webworker" />
+import { build, files, prerendered, version } from "$service-worker";
+import { precacheAndRoute } from "workbox-precaching";
 
-import { build, files, version } from "$service-worker";
+const precache_list = ["/", ...build, ...files, ...prerendered].map((s) => ({
+    url: s,
+    revision: version,
+}));
 
-const worker = self as unknown as ServiceWorkerGlobalScope;
-
-// Create a unique cache name for this deployment
-const CACHE = `cache-${version}`;
-
-const ASSETS = [
-    ...build, // the app itself
-    ...files, // everything in `static`
-];
-
-worker.addEventListener("install", (event) => {
-    // Create a new cache and add all files to it
-    async function addFilesToCache() {
-        const cache = await caches.open(CACHE);
-        await cache.addAll(ASSETS);
-    }
-
-    event.waitUntil(addFilesToCache());
-});
-
-worker.addEventListener("activate", (event) => {
-    // Remove previous cached data from disk
-    async function deleteOldCaches() {
-        for (const key of await caches.keys()) {
-            if (key !== CACHE) await caches.delete(key);
-        }
-    }
-
-    event.waitUntil(deleteOldCaches());
-});
-
-worker.addEventListener("fetch", async (event) => {
-    // ignore POST requests etc
-    if (event.request.method !== "GET") return;
-
-    async function respond() {
-        const url = new URL(event.request.url);
-        const cache = await caches.open(CACHE);
-
-        // `build`/`files` can always be served from the cache
-        if (ASSETS.includes(url.pathname)) {
-            return cache.match(event.request);
-        }
-
-        // for everything else, try the network first, but
-        // fall back to the cache if we're offline
-        try {
-            const response = await fetch(event.request);
-
-            if (response.status === 200) {
-                cache.put(event.request, response.clone());
-            }
-
-            return response;
-        } catch {
-            return cache.match(event.request);
-        }
-    }
-
-    const res = await respond();
-    if (res) event.respondWith(res);
-});
+precacheAndRoute(precache_list);
