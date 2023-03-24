ce734f971c86e32d58dae1b8ae9f4d70668aebb4
Laurent Stéphenne <laurent@guibi.ca>
2023-03-24 09:53:41 -0400

    Test avec le code de la doc de sveltekit

--- src/service-worker.ts
+++
@@ -1,9 +0,0 @@
-import { build, files, prerendered, version } from "$service-worker";
-import { precacheAndRoute } from "workbox-precaching";
-
-const precache_list = ["/", ...build, ...files, ...prerendered].map((s) => ({
-    url: s,
-    revision: version,
-}));
-
-precacheAndRoute(precache_list);
