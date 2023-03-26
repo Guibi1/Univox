9eaa58d11dd84c4fd2cd89fa16a068f0e1fade8a
Laurent Stéphenne <laurent@guibi.ca>
2023-03-26 11:47:26 -0400

    Fixed le merge

--- src/routes/(app)/amis/ajouter-ami/+page.svelte
+++ src/routes/(app)/amis/ajouter-ami/+page.svelte
@@ -1,4 +1,5 @@
-<script lang="ts" async>
+<script lang="ts">
+    import SearchBar from "$lib/components/SearchBar.svelte";
     import friends from "$lib/stores/friends";
     import type { User } from "$lib/Types";
 
@@ -9,71 +10,44 @@
         searchResults =
             query.length > 3 ? await (await fetch("/api/search/users/" + query)).json() : [];
     }
-
-    let timeout: NodeJS.Timeout | null;
-    function timedSearch() {
-        if (timeout) clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            handleSearch();
-        }, 500);
-    }
 </script>
 
 <svelte:head>
     <title>Univox | Ajouter des amis</title>
 </svelte:head>
 
-<h1 class="text-center pt-2 dark:bg-neutral-900">Ajouter des amis</h1>
+<h1 class="pt-2 text-center dark:bg-neutral-900">Ajouter des amis</h1>
 
 <div
-    class="top-0 z-50 p-6 grid grid-cols-3 gap-2 grid-justify-item-stretch border-b bg-white dark:bg-neutral-900 dark:border-neutral-500"
-> <!-- "sticky top-0 z-50 p-6 flex justify-center border-b bg-white dark:bg-neutral-900 dark:border-neutral-500" -->
-    <div class="grid grid-cols-2 gap-2 grid-justify-item-strech w-[1024px]"> <!-- "w-1/2 flex flex-row gap-3 items-center ml-10" -->
-        <input
-            type="text"
-            bind:value={query}
-            on:keypress={(e) => {
-                if (e.key == "Enter") handleSearch();
-            }}
-            on:input={timedSearch}
-            placeholder="Rechercher"
-            class="w-full h-12 rounded-lg text-lg"
-        />
+    class="sticky top-0 z-50 flex justify-center border-b bg-white p-6 dark:border-neutral-500 dark:bg-neutral-900"
+>
+    <div class="ml-10 flex w-1/2 flex-row items-center gap-3">
+        <SearchBar bind:query {handleSearch} />
 
-        <div ><box-icon
+        <box-icon
             name="search-alt"
-            class="col-start-1 col-end-3 w-10 h-10 cursor-pointer"
+            class="h-10 w-10 cursor-pointer"
             on:click={handleSearch}
             on:keypress={handleSearch}
         />
     </div>
-        
-    </div>
-    
 
-    <a class="col-start-4"
-    href=/amis
-    >
-        Liste d'amis
-        
-    </a>
+    <a href="/amis"> Liste d'amis </a>
 </div>
-
-<div class="grid grid-cols-[min-content_2fr_1fr] items-start gap-8 p-8" />
 
 {#each searchResults as user}
     <div>
         {user.lastName + ", " + user.firstName}
-        
-            <button class="filled"
-                on:click={() => {
-                    query = "";
-                    searchResults = [];
-                    friends.add(user._id);
-                }}
-            >
-                ajouter en ami
-            </button>
-        
+
+        <button
+            class="filled"
+            on:click={() => {
+                query = "";
+                searchResults = [];
+                friends.add(user._id);
+            }}
+        >
+            ajouter en ami
+        </button>
     </div>
 {/each}
