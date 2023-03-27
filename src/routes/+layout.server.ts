7a3ea67c6e96e6b151117e8ecf16b2242e934c73
Laurent Stéphenne <laurent@guibi.ca>
2023-03-27 15:19:53 -0400

    Préparation à intégrer les paramètres dans mongodb

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -11,16 +11,16 @@ export const load = (({ cookies, locals }) => {
     // Stores are up to date during SSR
     colorScheme.setInitial(color);
     startWeekDate.setInitial(startDate);
-    user.set(locals.user);
-    friends.set(locals.friends);
+    if (locals.user) {
+        user.set(locals.user);
+        friends.set(locals.friends);
+    }
 
     // Data available throughout the site
     return {
         storesInitialValue: {
             colorScheme: color,
             startWeekDate: startDate,
-            serializedUser: JSON.stringify(locals.user),
-            serializedFriends: JSON.stringify(locals.friends),
         },
     };
 }) satisfies LayoutServerLoad;
