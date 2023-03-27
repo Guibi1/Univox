d211ee5aa6eb967db0f55cdb3048333f88ea0afd
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-03-27 17:34:34 -0400

    Still the carousel
    Just one bug to fix

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -70,12 +70,13 @@
         use:dropFiles={images.length < maxImagesCount && !readOnly}
         on:dragenter={() => {isDragginOver = true}}
         on:dragleave={() => {isDragginOver = false}}
-        class={`relative flex h-64 items-stretch transition ease-in-out duration-300 ${isDragginOver?"bg-blue-secondary":"bg-neutral-700"}`}
+        
+        class={`relative flex h-64 items-stretch transition ease-in-out duration-300 ${isDragginOver ? "bg-blue-secondary":"bg-neutral-700"}`}
     >
         {#if images.length === 0}
             <label class="flex cursor-pointer flex-col items-center justify-center break-normal whitespace-nowrap">
                 <box-icon name="cloud-upload" class={`pointer-events-none transition ease-in-out duration-300 ${isDragginOver?"opacity-50":"opacity-100"}`} size="4rem" />
-                <p class={`pointer-events-none transition ease-in-out duration-300 ${isDragginOver?"opacity-50":"opacity-100"}`}>Glissez ici des images de votre livre</p>
+                <p class={`pointer-events-none ease-in-out duration-300 ${isDragginOver?"opacity-50":"opacity-100"}`}>Glissez ici des images de votre livre</p>
                 
 
                 <input
@@ -87,7 +88,12 @@
                 />
             </label>
         {:else}
-            <img src={images[selectedIndex]} alt="Livre" class="h-full w-full object-cover" />
+            <img src={images[selectedIndex]} alt="Livre" class={`pointer-events-none h-full w-full object-cover`} />
+            <div class={`flex w-full h-full absolute z-10 bg-blue-secondary flex-col items-center justify-center break-normal whitespace-nowrap transition ease-in-out duration-300 ${isDragginOver ? "opacity-100":"opacity-0"}`}>
+                <box-icon name="cloud-upload" class={`pointer-events-none transition ease-in-out duration-300 ${isDragginOver?"opacity-50":"opacity-100"}`} size="4rem" />
+                <p class={`pointer-events-none ease-in-out duration-300 ${isDragginOver?"opacity-20":"opacity-100"}`}>Glissez ici des images de votre livre</p>
+            </div>
+
 
             {#if !readOnly}
                 <button
@@ -102,12 +108,12 @@
     </div>
 
     <div class="relative h-10">
-        <div class="absolute -top-8 flex w-full items-center justify-center gap-2">
+        <div class="z-20 absolute -top-8 flex w-full items-center justify-center gap-2">
             {#each images as image, i}
                 <button type="button" on:click={() => (selectedIndex = i)}>
                     <img
                         src={image}
-                        class={`aspect-square w-20 cursor-pointer border-2 object-cover hover:scale-110 ${
+                        class={`aspect-square w-20 cursor-pointer border-2 object-cover transition ease-in-out duration-100 hover:scale-110 ${
                             selectedIndex === i ? "border-blue-primary" : "border-neutral-300"
                         }`}
                         alt=""
@@ -115,9 +121,10 @@
                 </button>
             {/each}
 
+            <!--TODO the problem with having the images half in the big rectangle is the drag and drop animaiton can bug if you leave by this square-->
             {#if images.length < maxImagesCount && !readOnly}
                 <label
-                    class="flex aspect-square w-20 cursor-pointer items-center justify-center border-2 border-neutral-300 bg-neutral-500 object-cover hover:scale-110"
+                    class="z-20 flex aspect-square w-20 cursor-pointer items-center justify-center border-2 border-neutral-300 bg-neutral-500 object-cover transition ease-in-out duration-100 hover:scale-110"
                 >
                     <box-icon class="absolute" name="image-add" size="2rem" />
                     <input
