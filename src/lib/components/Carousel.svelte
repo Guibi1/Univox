7c276e49e16d875c05f63a76fbd7b8fb34cb60e2
azom <97298292+azomwastaken@users.noreply.github.com>
2023-03-27 21:28:05 -0400

    Fixed silly mistake of @Guibi1 and fixed trash

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -68,16 +68,34 @@
 <div class="relative py-4">
     <div
         use:dropFiles={images.length < maxImagesCount && !readOnly}
-        on:dragenter={() => {isDragginOver = true}}
-        on:dragleave={() => {isDragginOver = false}}
-        
-        class={`relative flex h-64 items-stretch transition ease-in-out duration-300 ${isDragginOver ? "bg-blue-secondary":"bg-neutral-700"}`}
+        on:dragenter={() => {
+            isDragginOver = true;
+        }}
+        on:dragleave={() => {
+            isDragginOver = false;
+        }}
+        class={`relative flex h-64 items-stretch transition duration-300 ease-in-out ${
+            isDragginOver ? "bg-blue-secondary" : "bg-neutral-700"
+        }`}
     >
         {#if images.length === 0}
-            <label class="flex cursor-pointer flex-col items-center justify-center break-normal whitespace-nowrap">
-                <box-icon name="cloud-upload" class={`pointer-events-none transition ease-in-out duration-300 ${isDragginOver?"opacity-50":"opacity-100"}`} size="4rem" />
-                <p class={`pointer-events-none ease-in-out duration-300 ${isDragginOver?"opacity-50":"opacity-100"}`}>Glissez ici des images de votre livre</p>
-                
+            <label
+                class="flex cursor-pointer flex-col items-center justify-center whitespace-nowrap break-normal"
+            >
+                <box-icon
+                    name="cloud-upload"
+                    class={`pointer-events-none transition duration-300 ease-in-out ${
+                        isDragginOver ? "opacity-50" : "opacity-100"
+                    }`}
+                    size="4rem"
+                />
+                <p
+                    class={`pointer-events-none duration-300 ease-in-out ${
+                        isDragginOver ? "opacity-50" : "opacity-100"
+                    }`}
+                >
+                    Glissez ici des images de votre livre
+                </p>
 
                 <input
                     type="file"
@@ -88,18 +106,37 @@
                 />
             </label>
         {:else}
-            <img src={images[selectedIndex]} alt="Livre" class={`pointer-events-none h-full w-full object-cover`} />
-            <div class={`flex w-full h-full absolute z-10 bg-blue-secondary flex-col items-center justify-center break-normal whitespace-nowrap transition ease-in-out duration-300 ${isDragginOver ? "opacity-100":"opacity-0"}`}>
-                <box-icon name="cloud-upload" class={`pointer-events-none transition ease-in-out duration-300 ${isDragginOver?"opacity-50":"opacity-100"}`} size="4rem" />
-                <p class={`pointer-events-none ease-in-out duration-300 ${isDragginOver?"opacity-20":"opacity-100"}`}>Glissez ici des images de votre livre</p>
+            <img
+                src={images[selectedIndex]}
+                alt="Livre"
+                class={`pointer-events-none h-full w-full object-cover`}
+            />
+            <div
+                class={`absolute z-10 flex h-full w-full flex-col items-center justify-center whitespace-nowrap break-normal bg-blue-secondary transition duration-300 ease-in-out ${
+                    isDragginOver ? "opacity-100" : "opacity-0"
+                }`}
+            >
+                <box-icon
+                    name="cloud-upload"
+                    class={`pointer-events-none transition duration-300 ease-in-out ${
+                        isDragginOver ? "opacity-50" : "opacity-100"
+                    }`}
+                    size="4rem"
+                />
+                <p
+                    class={`pointer-events-none duration-300 ease-in-out ${
+                        isDragginOver ? "opacity-20" : "opacity-100"
+                    }`}
+                >
+                    Glissez ici des images de votre livre
+                </p>
             </div>
-
 
             {#if !readOnly}
                 <button
                     type="button"
                     on:click={() => removeImage(selectedIndex)}
-                    class="absolute top-2 right-2 fill-red-500"
+                    class="pointer-events-auto absolute top-2 right-2 z-20 fill-red-500"
                 >
                     <box-icon name="trash" size="2rem" />
                 </button>
@@ -108,12 +145,12 @@
     </div>
 
     <div class="relative h-10">
-        <div class="z-20 absolute -top-8 flex w-full items-center justify-center gap-2">
+        <div class="absolute -top-8 z-20 flex w-full items-center justify-center gap-2">
             {#each images as image, i}
                 <button type="button" on:click={() => (selectedIndex = i)}>
                     <img
                         src={image}
-                        class={`aspect-square w-20 cursor-pointer border-2 object-cover transition ease-in-out duration-100 hover:scale-110 ${
+                        class={`aspect-square w-20 cursor-pointer border-2 object-cover transition duration-100 ease-in-out hover:scale-110 ${
                             selectedIndex === i ? "border-blue-primary" : "border-neutral-300"
                         }`}
                         alt=""
@@ -124,7 +161,7 @@
             <!--TODO the problem with having the images half in the big rectangle is the drag and drop animaiton can bug if you leave by this square-->
             {#if images.length < maxImagesCount && !readOnly}
                 <label
-                    class="z-20 flex aspect-square w-20 cursor-pointer items-center justify-center border-2 border-neutral-300 bg-neutral-500 object-cover transition ease-in-out duration-100 hover:scale-110"
+                    class="z-20 flex aspect-square w-20 cursor-pointer items-center justify-center border-2 border-neutral-300 bg-neutral-500 object-cover transition duration-100 ease-in-out hover:scale-110"
                 >
                     <box-icon class="absolute" name="image-add" size="2rem" />
                     <input
