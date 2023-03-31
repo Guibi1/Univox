51697d2eae26603c8a0065245f5a5017cd4eeaae
azomwastaken <97298292+azomwastaken@users.noreply.github.com>
2023-03-31 10:55:35 -0400

    Reverted hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -12,6 +12,20 @@ const userLocalsHandle = (async ({ event, resolve }) => {
     return resolve(event);
 }) satisfies Handle;
 
+const settingsHandle = (async ({ event, resolve }) => {
+    if (!event.locals.user) {
+        event.cookies.delete("firstDayOfTheWeek");
+        return resolve(event);
+    }
+
+    event.cookies.set(
+        "firstDayOfTheWeek",
+        (await db.getSettings(event.locals.user))?.firstDayOfTheWeek ?? "Dimanche"
+    );
+
+    return resolve(event);
+}) satisfies Handle;
+
 const colorSchemeHandle = (async ({ event, resolve }) => {
     const colorScheme = event.cookies.get("colorScheme") ?? "dark";
     return resolve(event, {
