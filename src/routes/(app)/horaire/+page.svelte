c4636bfd8f247bc718c10e187e4ed882090ceecb
Laurent Stéphenne <laurent@guibi.ca>
2023-04-05 14:24:15 -0400

    Fixed errors

--- src/routes/(app)/horaire/+page.svelte
+++ src/routes/(app)/horaire/+page.svelte
@@ -1,10 +1,3 @@
-<script>
-    import { show } from "$lib/availabilities/commonAvailabilities";
-</script>
-
 <svelte:head>
     <title>Univox | Horaire</title>
 </svelte:head>
-
-<!--Button to test code for finding common availabilities-->
-<button on:click={show}> Test </button>
