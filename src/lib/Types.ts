2d41fdf9c000004dad04cdbc9c29fdf9848f1827
Virliott <123177639+Virliott@users.noreply.github.com>
2023-04-13 17:03:57 -0400

    conflits résolus

--- src/lib/Types.ts
+++ src/lib/Types.ts
@@ -9,7 +9,9 @@ export interface User {
     lastName: string;
     scheduleId: mongoose.Types.ObjectId;
     friendsId: mongoose.Types.ObjectId[];
+
     groupsId: mongoose.Types.ObjectId[];
+
     settingsId: mongoose.Types.ObjectId;
 }
 
--- src/lib/server/models/users.ts
+++ src/lib/server/models/users.ts
@@ -48,6 +48,7 @@ const UserSchema = new Schema<ServerUser>({
     },
     groupsId: {
         type: [mongoose.Schema.Types.ObjectId],
+
         validate: {
             validator: function (arr: unknown[]) {
                 return arr.length === new Set(arr).size;
