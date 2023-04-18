e40e7c2c4cbfeaf0a165e212c3936b7ab6359f35
Laurent Stéphenne <laurent@guibi.dev>
2023-04-18 16:02:03 -0400

    Fini l'horaire

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import { invalidate } from "$app/navigation";
     import friends from "$lib/stores/friends";
-    import notifications from "$lib/stores/notifications";
     import groups from "$lib/stores/groups";
+    import notifications from "$lib/stores/notifications";
+    import schedule from "$lib/stores/schedule";
     import user from "$lib/stores/user";
     import { onMount } from "svelte";
     import type { LayoutData } from "./$types";
@@ -10,9 +11,10 @@
 
     export let data: LayoutData;
     user.set(JSON.parse(data.storesInitialValue.serializedUser));
+    schedule.set(JSON.parse(data.storesInitialValue.serializedSchedule));
     friends.set(JSON.parse(data.storesInitialValue.serializedFriends));
-    notifications.set(JSON.parse(data.storesInitialValue.serializedNotifications));
     groups.set(JSON.parse(data.storesInitialValue.serializedGroups));
+    notifications.set(JSON.parse(data.storesInitialValue.serializedNotifications));
 
     onMount(() => {
         const bc = new BroadcastChannel("Invalidate user");
