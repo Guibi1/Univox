662fdf564009ab5260f75e6923af3628668d1219
Laurent Stéphenne <laurent@guibi.dev>
2023-04-22 17:52:15 -0400

    Forgor in last commit

--- src/routes/(app)/livres/mes-livres/+page.server.ts
+++ src/routes/(app)/livres/mes-livres/+page.server.ts
@@ -1,6 +1,7 @@
+import { arrayIdToString } from "$lib/sanitization";
 import * as db from "$lib/server/db";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async ({ locals }) => {
-    return { books: db.arrayIdToString(await db.getBooks(locals.user)) };
+    return { books: arrayIdToString(await db.getBooks(locals.user)) };
 }) satisfies PageServerLoad;
