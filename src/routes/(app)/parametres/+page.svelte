3dfe36b353427bd0acedd855080642af403df116
Laurent Stéphenne <laurent@guibi.ca>
2023-04-23 00:33:52 -0400

    Petits changements dans la page de paramètres

--- src/routes/(app)/parametres/+page.svelte
+++ src/routes/(app)/parametres/+page.svelte
@@ -1,90 +1,69 @@
 <script lang="ts">
-    import firstDayOfTheWeek from "$lib/stores/firstDayOfTheWeek";
-    import ColorSchemeSwitch from "$lib/components/ColorSchemeSwitch.svelte";
-    import Select from "$lib/components/Select.svelte";
     import Option from "$lib/components/Option.svelte";
+    import Select from "$lib/components/Select.svelte";
+    import firstDayOfTheWeek from "$lib/stores/firstDayOfTheWeek";
 
     import Avatar from "$lib/components/Avatar.svelte";
-    import user from "$lib/stores/user";
     import colorScheme from "$lib/stores/colorScheme";
+    import user from "$lib/stores/user";
 
     const refresh = () => user.setAvatar(Math.random() + $user.firstName + $user.da);
 </script>
 
-<div class="grid grid-cols-[1fr_2fr]">
-    <div class="m-auto flex w-fit gap-4">
-        <div class="flex flex-col">
+<div class="mx-auto grid grid-cols-[max-content_40rem] gap-6 divide-x px-10 py-4">
+    <div class="flex flex-col items-center gap-2 text-center">
+        <div class="h-32">
             <Avatar />
-
-            <button class="filled gap-2" on:click={refresh}>
-                <i class="bx bx-dice-6 text-2xl" />
-                Avatar au hasard
-            </button>
         </div>
 
-        <div class="flex flex-col items-start gap-1 pt-6">
-            <h1>
-                {$user.firstName}
-                {$user.lastName}
-            </h1>
+        <button class="outlined gap-2" on:click={refresh}>
+            <i class="bx bx-dice-6 text-2xl" />
+            Avatar au hasard
+        </button>
 
-            <span class="flex items-center gap-1">
-                <i class="bx bx-map text-2xl" />
-                Étudiant au Collège Bois de Boulogne
-            </span>
+        <h1>
+            {$user.firstName}
+            {$user.lastName}
+        </h1>
 
-            <span>
-                Numéro de DA :
-                <b>{$user.da}</b>
-            </span>
+        <span class="flex items-center justify-center gap-2">
+            <i class="bx bx-map text-2xl" />
+            Étudiant au Collège Bois de Boulogne
+        </span>
 
-            <a href={"mailto:" + $user.email} class="flex items-center gap-1">
-                <i class="bx bx-envelope text-2xl" />
-                {$user.email}
-            </a>
-        </div>
+        <span>
+            Numéro de DA :
+            <b>{$user.da}</b>
+        </span>
+
+        <a href={"mailto:" + $user.email} class="flex items-center justify-center gap-2">
+            <i class="bx bx-envelope text-2xl" />
+            {$user.email}
+        </a>
     </div>
 
-    <div>
-        <h1 class="my-4 text-center">Paramètres</h1>
-        <hr class="my-4" />
-        <div class="my-4 flex flex-col items-center">
-            <div class="my-4 flex items-center space-x-32">
-                <div>
-                    <Select bind:value={$firstDayOfTheWeek}>
-                        <Option text="Lundi" />
-                        <Option text="Samedi" />
-                        <Option text="Dimanche" />
-                    </Select>
-                </div>
-                <span class="ml-2 text-2xl">Jour de commencement de la semaine</span>
-            </div>
+    <div class="pl-6">
+        <h1 class="my-4 gap-4 border-b-2 pb-2">Paramètres</h1>
 
-            <div class="my-4 flex items-center space-x-32">
-                <div>
-                    <Select bind:value={$colorScheme}>
-                        <Option text="dark" onClick={() => console.log("Hi")} />
-                        <Option text="light" />
-                    </Select>
-                </div>
-                <span class="ml-2 text-2xl">Jour de commencement de la semaine</span>
-            </div>
+        <div class="grid grid-cols-2 items-center gap-x-2 gap-y-4">
+            <Select bind:value={$firstDayOfTheWeek}>
+                <Option text="Lundi" />
+                <Option text="Samedi" />
+                <Option text="Dimanche" />
+            </Select>
+            <span class="ml-2 text-2xl">Premier jour de la semaine</span>
 
-            <div class="my-4 flex items-center space-x-32">
-                <div>
-                    <Select bind:value={$firstDayOfTheWeek}>
-                        <Option text="Lundi" />
-                        <Option text="Samedi" />
-                        <Option text="Dimanche" />
-                    </Select>
-                </div>
-                <span class="ml-2 text-2xl">Jour de commencement de la semaine</span>
-            </div>
+            <Select bind:value={$colorScheme}>
+                <Option text="Foncé" value="dark" />
+                <Option text="Clair" value="light" />
+            </Select>
+            <span class="ml-2 text-2xl">Thème du site</span>
+
+            <input type="email" name="email" value={$user.email} readonly />
+            <span class="ml-2 text-2xl">Adresse courriel</span>
         </div>
     </div>
 </div>
 
-<ColorSchemeSwitch />
-
 <!--Pour donner un sens de sécurité aux utilisateurs-->
-<a href="/">Sauvgarder et quitter</a>
+<a href="/" class="outlined self-center">Sauvgarder et quitter</a>
