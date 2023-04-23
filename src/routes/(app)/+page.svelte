c5ccc02cdc023d11d97685b369e2400376205de9
alexandreVeregut <123177507+alexandreVeregut@users.noreply.github.com>
2023-04-23 12:44:30 -0400

    Small changes towords finishing ui

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,118 +1,29 @@
 <script lang="ts">
     import ScheduleView from "$lib/components/ScheduleView.svelte";
-    import type { Book, Class, User } from "$lib/Types";
+    import type { Book, Class, Schedule, User } from "$lib/Types";
     import dayjs from "dayjs";
     import mongoose from "mongoose";
     import friends from "$lib/stores/friends";
+    import Avatar from "$lib/components/Avatar.svelte";
+    import user from "$lib/stores/user";
 
     let userNameOfSchedule: string = "L'horaire de : Master Chief";
 
-    let schedule: Class[] = [
+    let books: Book[] = [
         {
             _id: new mongoose.Types.ObjectId(),
-            code: "601-101-MQ",
-            name: "Yey c'est le 2",
-            group: 105,
-            local: "B-105",
-            type: "L",
-            teacher: "asdjkfwlbk",
-            virtual: false,
-            timeStart: dayjs().date(2).hour(6).minute(0),
-            timeEnd: dayjs().date(2).hour(8).minute(0),
-        },
-        {
-            _id: new mongoose.Types.ObjectId(),
-            code: "601-101-MQ",
-            name: "aName",
-            group: 105,
-            local: "B-105",
-            type: "L",
-            teacher: "asdjkfwlbk",
-            virtual: false,
-            timeStart: dayjs().hour(14).minute(0),
-            timeEnd: dayjs().hour(16).minute(0),
-        },
-        {
-            _id: new mongoose.Types.ObjectId(),
-            code: "601-101-MQ",
-            name: "BName",
-            group: 105,
-            local: "B-105",
-            type: "L",
-            teacher: "asdjkfwlbk",
-            virtual: false,
-            timeStart: dayjs().weekday(6).hour(16).minute(30),
-            timeEnd: dayjs().weekday(6).hour(20).minute(0),
+            code: "hello",
+            sellerId: new mongoose.Types.ObjectId(),
+            title: "Bruh",
+            ISBN: "Ok",
+            src: [
+                "https://en.wikipedia.org/wiki/Image#/media/File:Image_created_with_a_mobile_phone.png",
+            ],
+            author: "Amogus",
+            price: 69,
+            state: "Available",
         },
     ];
-
-    let books: Book[] = [];
-
-    //##############################################################################
-    /*JE SUIS RENDU À FAIRE EN SORTE QUE LES GETTERS PRENNENT
-    LE ID AU GENS SPÉCIFIQUE AVEC LA BASE DE DONNÉES
-    
-    IL FAUT AUSSI TROUVER UNE FAÇON DE METTRE DE GETTERS DANS stores/user et friends
-    POUR NE PAS RÉÉCRIRE LE GETTERS DES USER DANS CETTE PAGE POUR RIEN*/
-    //##############################################################################
-
-    //Gets the schedule that needs to be shown (ex : if you click on your friends schedule, it will be shown)
-    function getSchedule(_id: mongoose.Types.ObjectId) {
-        schedule = [
-            {
-                _id: new mongoose.Types.ObjectId(),
-                code: "bruh",
-                name: "bruh",
-                group: 105,
-                local: "B-105",
-                type: "L",
-                teacher: "asdjkfwlbk",
-                virtual: false,
-                timeStart: dayjs().weekday(2).hour(8).minute(0),
-                timeEnd: dayjs().weekday(2).hour(11).minute(0),
-            },
-            {
-                _id: new mongoose.Types.ObjectId(),
-                code: "you",
-                name: "you",
-                group: 105,
-                local: "B-105",
-                type: "L",
-                teacher: "asdjkfwlbk",
-                virtual: false,
-                timeStart: dayjs().weekday(1).hour(10).minute(0),
-                timeEnd: dayjs().weekday(1).hour(20).minute(0),
-            },
-            {
-                _id: new mongoose.Types.ObjectId(),
-                code: "stupid",
-                name: "stupid",
-                group: 105,
-                local: "B-105",
-                type: "L",
-                teacher: "asdjkfwlbk",
-                virtual: false,
-                timeStart: dayjs().weekday(6).hour(20).minute(0),
-                timeEnd: dayjs().weekday(6).hour(24).minute(0),
-            },
-        ];
-    }
-
-    function getProfil(_id: mongoose.Types.ObjectId) {
-        //À REMPLIR
-    }
-
-    //Gets the name of the user from the schedule that is being shown
-    function getUser(_id: mongoose.Types.ObjectId) {
-        return {
-            _id: new mongoose.Types.ObjectId(),
-            da: "1234567",
-            email: "no",
-            firstName: "Leroy",
-            lastName: "Jenkins",
-            avatar: "Rabbit",
-        };
-    }
 </script>
 
 <svelte:head>
@@ -127,7 +38,7 @@
     <div class="flex flex-col gap-2">
         <h1 class="text-center font-bold">{userNameOfSchedule}</h1>
         <!--This is where the schedule is-->
-        <ScheduleView {schedule} />
+        <!--<ScheduleView {schedule} />--->
     </div>
     <div class="flex flex-col gap-2">
         <h1 class="text-center">Amis</h1>
@@ -136,17 +47,14 @@
             class="flex h-[20rem] flex-col gap-3 overflow-y-scroll rounded-md border-2 border-gray-300 p-3"
         >
             <!--Loop through the friends-->
-            {#each $friends as ami}
+            {#each $friends as friend}
                 <!--Make the friends have some space in between them-->
                 <div class="flex flex-row justify-between">
                     <div class="flex flex-row gap-2">
-                        <img
-                            src={getUser(new mongoose.Types.ObjectId()).avatar}
-                            class="h-10 w-10 rounded-full"
-                        />
+                        <div class="h-10 w-10"><Avatar /></div>
                         <div class="flex flex-col">
-                            <div>{ami.lastName + ", " + ami.firstName}</div>
-                            <div class="text-sm text-gray-500">{ami.email}</div>
+                            <div>{friend.lastName + ", " + friend.firstName}</div>
+                            <div class="text-sm text-gray-500">{friend.email}</div>
                         </div>
                     </div>
                     <div class="flex flex-row gap-2">
@@ -154,21 +62,12 @@
                             class="rounded bg-blue-primary px-4 py-2 font-bold hover:bg-blue-primary"
                             on:click={() => {
                                 userNameOfSchedule =
-                                    "L'horaire de : " +
-                                    getUser(new mongoose.Types.ObjectId()).firstName +
-                                    " " +
-                                    getUser(new mongoose.Types.ObjectId()).lastName;
-                                getSchedule(new mongoose.Types.ObjectId());
+                                    "L'horaire de : " + $user.firstName + " " + $user.lastName;
                             }}
                         >
                             Horaire
                         </button>
-                        <button
-                            class="rounded bg-blue-500 px-4 py-2 font-bold hover:bg-blue-700"
-                            on:click={() => {
-                                getProfil(new mongoose.Types.ObjectId());
-                            }}
-                        >
+                        <button class="rounded bg-blue-500 px-4 py-2 font-bold hover:bg-blue-700">
                             Profil
                         </button>
                     </div>
@@ -187,10 +86,7 @@
                     <!--Make the books have some space in between them-->
                     <div class="flex flex-row justify-between">
                         <div class="flex flex-row gap-2">
-                            <img
-                                src="https://www.gravatar.com/avatar"
-                                class="h-10 w-10 rounded-full"
-                            />
+                            <img src={book.src[0]} class="h-10 w-10 rounded-full" />
                             <div class="flex flex-col">
                                 <div>{book.title}</div>
                                 <div class="text-sm text-gray-500">{book.author}</div>
