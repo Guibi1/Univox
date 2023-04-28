bb8237d09f6634b76d6ea6d80bffa32c4b8c721e
maxiguil <maxime.rodgers@gmail.com>
2023-04-28 12:06:27 -0400

    Broken changer email
    Co-authored-by: azom <azomwastaken@users.noreply.github.com>

--- src/routes/(app)/parametres/+page.svelte
+++ src/routes/(app)/parametres/+page.svelte
@@ -8,10 +8,18 @@
     import user from "$lib/stores/user";
 
     const refresh = () => user.setAvatar(Math.random() + $user.firstName + $user.da);
+
+    // Add a temporary email variable
+    let tempEmail = $user.email;
+
+    // Update this function to handle email update
+    const updateEmail = async () => {
+        user.setEmail(tempEmail);
+    };
 </script>
 
-<div class="mx-auto grid gap-6 py-4 laptop:grid-cols-[max-content_40rem] laptop:divide-x">
-    <div class="flex flex-col items-center gap-2 text-center">
+<div class="mx-auto mb-5 grid gap-6 py-4 laptop:grid-cols-[max-content_40rem] laptop:divide-x">
+    <div class="flex flex-col items-center gap-2 space-y-4 text-center">
         <div class="h-32">
             <Avatar />
         </div>
@@ -42,16 +50,16 @@
         </a>
     </div>
 
-    <div class="laptop:pl-6">
-        <h1 class="my-4 gap-4 border-b-2 pb-2">Paramètres</h1>
+    <div class="space-x-14 space-y-10 laptop:pl-6">
+        <h1 class="my-4 flex justify-center gap-4 border-b-2 pb-2">Paramètres</h1>
 
-        <div class="grid grid-cols-2 items-center gap-x-2 gap-y-4">
+        <div class="grid grid-cols-2 items-center gap-x-16 gap-y-16">
             <Select bind:value={$firstDayOfTheWeek}>
                 <Option text="Lundi" />
                 <Option text="Samedi" />
                 <Option text="Dimanche" />
             </Select>
-            <span class="ml-2 text-2xl">Premier jour de la semaine</span>
+            <span class="ml-2 whitespace-nowrap text-2xl">Premier jour de la semaine</span>
 
             <Select bind:value={$colorScheme}>
                 <Option text="Foncé" value="dark" />
@@ -59,11 +67,17 @@
             </Select>
             <span class="ml-2 text-2xl">Thème du site</span>
 
-            <input type="email" name="email" value={$user.email} readonly />
+            <!-- Make the email input field editable and bind its value to tempEmail -->
+            <input type="email" name="email" bind:value={tempEmail} />
             <span class="ml-2 text-2xl">Adresse courriel</span>
+
+            <!-- Add the button to change email right under the email field -->
+            <button class="outlined col-span-2 gap-2" on:click={updateEmail}>
+                Changer l'adresse courriel
+            </button>
         </div>
     </div>
 </div>
 
 <!--Pour donner un sens de sécurité aux utilisateurs-->
-<a href="/" class="outlined self-center">Sauvgarder et quitter</a>
+<a href="/" class="outlined justify-bottom flex self-center">Sauvgarder et quitter</a>
