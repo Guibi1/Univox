3b73a1372fdd7c1c233d818c60d2fd9370be4d06
Laurent Stéphenne <laurent@guibi.dev>
2023-04-28 11:16:02 -0400

    La recherche de users est server side :)

--- src/routes/(app)/amis/+page.server.ts
+++ src/routes/(app)/amis/+page.server.ts
@@ -1,15 +1,35 @@
-import { objectIdToString } from "$lib/sanitization";
+import type { User } from "$lib/Types";
+import { arrayIdToString, objectIdToString } from "$lib/sanitization";
 import * as db from "$lib/server/db";
 import { isObjectIdOrHexString } from "mongoose";
 import type { PageServerLoad } from "./$types";
 
-export const load = (async ({ locals, url }) => {
-    const userId = url.searchParams.get("id");
-    if (!userId || !isObjectIdOrHexString(userId)) return;
-    if (!locals.user.friendsId.some((id) => id.equals(userId))) return;
+export const load = (async ({ locals, url, depends }) => {
+    depends("app:notifications");
 
-    const user = await db.getServerUser(userId);
-    if (!user) return;
+    const query = url.searchParams.get("query") ?? "";
+    const friendId = url.searchParams.get("friendId") ?? "";
 
-    return { schedule: objectIdToString(await db.getSchedule(user)) };
+    let friend;
+    if (
+        isObjectIdOrHexString(friendId) &&
+        locals.user.friendsId.some((id) => id.equals(friendId))
+    ) {
+        friend = await db.getServerUser(friendId);
+    }
+
+    const result: { user: User; friendRequestSent: boolean }[] = [];
+
+    const users = await db.searchUsers(locals.user, query);
+    for (const user of users) {
+        const friendRequestSent = await db.friendRequestExists(locals.user, user);
+        result.push({ user: db.serverUserToUser(user), friendRequestSent });
+    }
+
+    return {
+        query,
+        friendId,
+        schedule: friend ? objectIdToString(await db.getSchedule(friend)) : null,
+        searchResults: arrayIdToString(result),
+    };
 }) satisfies PageServerLoad;
