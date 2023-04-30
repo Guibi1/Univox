d29810df60f5edc473c79d4d846f08fc36089d3b
Laurent Stéphenne <laurent@guibi.ca>
2023-04-30 14:03:31 -0400

    L'horaire utilise superforms

---
+++ src/routes/(app)/horaire/importer/+page.server.ts
@@ -0,0 +1,38 @@
+import { importSchema } from "$lib/formSchema";
+import * as db from "$lib/server/db";
+import * as omnivox from "$lib/server/omnivox";
+import { fail, redirect } from "@sveltejs/kit";
+import dayjs from "dayjs";
+import { setError, superValidate } from "sveltekit-superforms/server";
+import type { Actions } from "./$types";
+
+export async function load() {
+    const form = await superValidate(importSchema);
+    return { form };
+}
+
+export const actions = {
+    import: async ({ request, locals }) => {
+        const form = await superValidate(request, importSchema);
+
+        if (!form.valid) {
+            return fail(400, { form });
+        }
+
+        try {
+            const cookie = await omnivox.login(locals.user.email, form.data.omnivoxPassword);
+            const html = await omnivox.fetchSchedulePageHTML(
+                cookie,
+                dayjs().year(),
+                omnivox.Semester.Winter
+            );
+            const schedule = omnivox.schedulePageToClasses(html);
+
+            await db.addClassesToSchedule(locals.user, schedule);
+        } catch (e) {
+            return setError(form, "omnivoxPassword", "Mot de passe erroné");
+        }
+
+        throw redirect(302, "/horaire");
+    },
+} satisfies Actions;
