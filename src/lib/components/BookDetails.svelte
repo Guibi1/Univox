6c40e80e012de6ca74e96067f59c362dc8722757
alexandreVeregut <123177507+alexandreVeregut@users.noreply.github.com>
2023-05-05 17:36:55 -0400

    Finished ui mes livres (supposedly)

--- src/lib/components/BookDetails.svelte
+++ src/lib/components/BookDetails.svelte
@@ -5,14 +5,46 @@
     export let book: Book | null = null;
 </script>
 
-<div class="flex flex-col justify-center">
+<div class="flex flex-col items-center">
     {#if book}
         <Carousel images={book.src} readOnly />
+        <div class="grid grid-cols-[4fr_0px] gap-2">
+            <div class="flex flex-col text-center">
+                <span>
+                    <span class="text-xl font-bold">
+                        {book.title}
+                    </span>
+                    <span class="font-bold italic">
+                        de {book.author}
+                    </span>
+                </span>
 
-        <div>
-            <h3>
-                {book.title}
-            </h3>
+                <span>
+                    Pour
+                    <span class="underline">
+                        {book.code}
+                    </span>
+                </span>
+
+                <span>
+                    Est {book.state}
+                </span>
+
+                <span>
+                    En vente pour {book.price} $
+                </span>
+
+                <span>
+                    ISBN : {book.ISBN}
+                </span>
+            </div>
+            <span>
+                <button
+                    class="flex h-8 w-8 items-center justify-center rounded-lg bg-red-500 text-white hover:bg-red-900"
+                >
+                    <i class="bx bx-trash" />
+                </button>
+            </span>
         </div>
     {:else}
         <span>Pas de livre sélectionné</span>
