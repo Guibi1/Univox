184278eb7727217401f41644756d17959baa5b29
azomDev <azom.developer@gmail.com>
2023-05-13 21:15:42 -0400

    Can delete book

--- src/routes/(app)/livres/mes-livres/+page.server.ts
+++ src/routes/(app)/livres/mes-livres/+page.server.ts
@@ -2,6 +2,7 @@ import { arrayIdToString } from "$lib/sanitization";
 import * as db from "$lib/server/db";
 import type { PageServerLoad } from "./$types";
 
-export const load = (async ({ locals }) => {
+export const load = (async ({ locals, depends }) => {
+    depends("books");
     return { books: arrayIdToString(await db.getBooks(locals.user)) };
 }) satisfies PageServerLoad;
