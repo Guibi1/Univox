c5767603a08ad803bd09bdcb09c9ab6cb1a9cd79
Laurent Stéphenne <laurent@guibi.dev>
2023-07-07 13:17:13 -0400

    Package cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,16 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
     preprocess: vitePreprocess(),
-    kit: { adapter: adapter() },
+    kit: {
+        adapter: adapter(),
+        alias: {
+            "$lib": "./src/lib",
+            "$lib/*": "./src/lib/*",
+            "$src/*": "./src/*",
+            "$assets/*": "./src/assets/*",
+        },
+    },
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import path from "path";
-import type { UserConfig } from "vite";
+import { apiFetch } from "sveltekit-api-fetch/vite";
+import { defineConfig } from "vite";
 
-const config: UserConfig = {
-    plugins: [sveltekit()],
-    define: {
-        "process.env.NODE_ENV": '"production"',
-    },
-    resolve: {
-        alias: {
-            $lib: path.resolve("./src/lib"),
-            $src: path.resolve("./src"),
-            $assets: path.resolve("./src/assets"),
-        },
-    },
-};
-
-export default config;
+export default defineConfig({
+    plugins: [sveltekit(), apiFetch()],
+});
